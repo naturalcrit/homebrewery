c93c6b13c40e1e61f64aa7303dbc552e9de46326
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2016-04-06 00:15:15 -0400

    Page container is now doign partial rendering, need to clean up the stlying though

--- client/homebrew/pageContainer/pageContainer.jsx
+++ client/homebrew/pageContainer/pageContainer.jsx
@@ -4,23 +4,52 @@ var cx = require('classnames');
 
 var Markdown = require('marked');
 
+var PAGE_HEIGHT = 1056 + 30;
+
 var PageContainer = React.createClass({
 	getDefaultProps: function() {
 		return {
 			text : ""
 		};
 	},
+	getInitialState: function() {
+		return {
+			scrollPosition: 0
+		};
+	},
+
+	handleScroll : function(e){
+		this.setState({
+			scrollPosition : e.target.scrollTop
+		});
+	},
+
+	getViewablePageIndex : function(){
+		return Math.floor(this.state.scrollPosition / PAGE_HEIGHT);
+	},
+
+	renderDummyPage : function(key){
+		return <div className='phb' key={key}>
+			yo dawg
+		</div>
+	},
 
 	renderPages : function(){
+		var currentIndex = this.getViewablePageIndex();
 		return _.map(this.props.text.split('\\page'), (pageText, index) => {
-			return <div className='phb' dangerouslySetInnerHTML={{__html:Markdown(pageText)}} key={index} />
+			if(currentIndex - 1 == index || currentIndex == index || currentIndex + 1 == index){
+				return <div className='phb' dangerouslySetInnerHTML={{__html:Markdown(pageText)}} key={index} />
+			}else{
+				return this.renderDummyPage(index);
+			}
 		})
 	},
 
 	render : function(){
-		var self = this;
-		return <div className="pageContainer">
-			{this.renderPages()}
+		return <div className="pageContainer" onScroll={this.handleScroll}>
+			<div className='pages'>
+				{this.renderPages()}
+			</div>
 		</div>;
 	}
 });
