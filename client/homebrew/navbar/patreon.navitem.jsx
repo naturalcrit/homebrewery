0612fc63e6b20330b6e5dd387efa80b47efec1dd
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2016-05-16 21:17:02 -0400

    Chagne the help out icon to a heart that glows red

--- client/homebrew/navbar/patreon.navitem.jsx
+++ client/homebrew/navbar/patreon.navitem.jsx
@@ -2,7 +2,12 @@ var React = require('react');
 var Nav = require('naturalcrit/nav/nav.jsx');
 
 module.exports = function(props){
-	return <Nav.item newTab={true} href='https://www.patreon.com/stolksdorf' color='green' icon='fa-gift'>
+	return <Nav.item
+		className='patreon'
+		newTab={true}
+		href='https://www.patreon.com/stolksdorf'
+		color='green'
+		icon='fa-heart'>
 		help out
 	</Nav.item>
 };
