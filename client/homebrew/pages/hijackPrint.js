14b7d608565268c3693df95ad10f9f6be2c70f4f
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2016-05-25 00:24:42 -0400

    Adding in mac support for print hijack

--- client/homebrew/pages/hijackPrint.js
+++ client/homebrew/pages/hijackPrint.js
@@ -1,7 +1,7 @@
 module.exports = function(shareId){
 	return function(event){
 		event = event || window.event;
-		if(event.ctrlKey && event.keyCode == 80){
+		if((event.ctrlKey || event.metaKey) && event.keyCode == 80){
 			var win = window.open(`/homebrew/print/${shareId}?dialog=true`, '_blank');
 			win.focus();
 			event.preventDefault();
