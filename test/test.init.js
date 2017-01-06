7b38bccec10bcedfa12f0c024926fed69df8dafa
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2017-01-06 01:34:34 -0500

    config file for tests

--- test/test.init.js
+++ test/test.init.js
@@ -1,8 +1,15 @@
 require('app-module-path').addPath('./server');
 
+const config = require('nconf')
+	.argv()
+	.env({ lowerCase: true })
+	.file('testing', { file: `test/config.json` })
+	.file('environment', { file: `config/${process.env.NODE_ENV}.json` })
+	.file('defaults', { file: 'config/default.json' });
+
 const should = require('chai').use(require('chai-as-promised')).should();
 const log = require('loglevel');
-log.setLevel('trace');
+log.setLevel(config.get('log_level'));
 
 module.exports = {
 	should: should
