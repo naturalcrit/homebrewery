bfb2cea48e89449ba069ec06674d56d732a639cb
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2017-02-28 23:21:41 -0500

    Working on onster block

--- shared/homebrewery/snippets/brew/index.js
+++ shared/homebrewery/snippets/brew/index.js
@@ -4,7 +4,8 @@ module.exports = _.merge(
 	require('./spell.snippet.js'),
 	require('./table.snippet.js'),
 	require('./classTable.snippet.js'),
-	require('./note.snippet.js')
+	require('./note.snippet.js'),
+	require('./monster.snippet.js')
 
 
 		//wide
