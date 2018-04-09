40f1d4c7908a49868114b73992e76cf616462e34
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2018-04-08 22:10:40 -0400

    Updating navbar link to go to the new subreddit

--- client/homebrew/navbar/issue.navitem.jsx
+++ client/homebrew/navbar/issue.navitem.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 var Nav = require('naturalcrit/nav/nav.jsx');
 
 module.exports = function(props){
-	return <Nav.item newTab={true} href='https://github.com/stolksdorf/homebrewery/issues' color='red' icon='fa-bug'>
+	return <Nav.item newTab={true} href='https://www.reddit.com/r/homebrewery/submit?selftext=true&title=[Issue]' color='red' icon='fa-bug'>
 		report issue
 	</Nav.item>
 };
