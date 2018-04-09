f4985b68ca2411139ba89946ed19f08cda25bfe9
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2018-04-08 23:36:26 -0400

    Updating build scripts and libraries

--- scripts/build.js
+++ scripts/build.js
@@ -2,19 +2,19 @@ const label = 'build';
 console.time(label);
 
 const clean = require('vitreum/steps/clean.js');
-const jsx   = require('vitreum/steps/jsx.js').partial;
-const lib   = require('vitreum/steps/libs.js').partial;
-const less  = require('vitreum/steps/less.js').partial;
-const asset = require('vitreum/steps/assets.js').partial;
+const jsx   = require('vitreum/steps/jsx.js');
+const lib   = require('vitreum/steps/libs.js');
+const less  = require('vitreum/steps/less.js');
+const asset = require('vitreum/steps/assets.js');
 
 const Proj = require('./project.json');
 
 clean()
 	.then(lib(Proj.libs))
-	.then(jsx('homebrew', './client/homebrew/homebrew.jsx', Proj.libs, ['./shared']))
-	.then(less('homebrew', ['./shared']))
-	.then(jsx('admin', './client/admin/admin.jsx', Proj.libs, ['./shared']))
-	.then(less('admin', ['./shared']))
-	.then(asset(Proj.assets, ['./shared', './client']))
+	.then(()=>jsx('homebrew', './client/homebrew/homebrew.jsx', {libs: Proj.libs, shared : ['./shared']}))
+	.then((deps)=>less('homebrew', {shared: ['./shared']}, deps))
+	.then(()=>jsx('admin', './client/admin/admin.jsx', {libs: Proj.libs, shared: ['./shared']}))
+	.then((deps)=>less('admin', {shared: ['./shared']}, deps))
+	.then(()=>asset(Proj.assets, ['./shared', './client']))
 	.then(console.timeEnd.bind(console, label))
 	.catch(console.error);
--- scripts/dev.js
+++ scripts/dev.js
@@ -1,23 +1,22 @@
 const label = 'dev';
 console.time(label);
 
-const jsx = require('vitreum/steps/jsx.watch.js').partial;
-const less = require('vitreum/steps/less.watch.js').partial;
-const assets = require('vitreum/steps/assets.watch.js').partial;
-const server = require('vitreum/steps/server.watch.js').partial;
-const livereload = require('vitreum/steps/livereload.js').partial;
+const jsx = require('vitreum/steps/jsx.watch.js');
+const less = require('vitreum/steps/less.watch.js');
+const assets = require('vitreum/steps/assets.watch.js');
+const server = require('vitreum/steps/server.watch.js');
+const livereload = require('vitreum/steps/livereload.js');
 
 const Proj = require('./project.json');
 
 Promise.resolve()
-	.then(jsx('homebrew', './client/homebrew/homebrew.jsx', Proj.libs, './shared'))
-	.then(less('homebrew', './shared'))
+	.then(()=>jsx('homebrew', './client/homebrew/homebrew.jsx', {libs: Proj.libs, shared : ['./shared']}))
+	.then((deps)=>less('homebrew', {shared: ['./shared']}, deps))
+	.then(()=>jsx('admin', './client/admin/admin.jsx', {libs: Proj.libs, shared: ['./shared']}))
+	.then((deps)=>less('admin', {shared: ['./shared']}, deps))
 
-	.then(jsx('admin', './client/admin/admin.jsx', Proj.libs, './shared'))
-	.then(less('admin', './shared'))
-
-	.then(assets(Proj.assets, ['./shared', './client']))
-	.then(livereload())
-	.then(server('./server.js', ['server']))
+	.then(()=>assets(Proj.assets, ['./shared', './client']))
+	.then(()=>livereload())
+	.then(()=>server('./server.js', ['server']))
 	.then(console.timeEnd.bind(console, label))
 	.catch(console.error);
