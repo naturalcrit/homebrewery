ede3647980aec8e5b6e24e0cc03df4ed3e6d19e3
Trevor Buckner <calculuschild@gmail.com>
2018-05-10 12:59:26 -0400

    Lint Fixes 2?

--- client/homebrew/brewRenderer/brewRenderer.jsx
+++ client/homebrew/brewRenderer/brewRenderer.jsx
@@ -156,8 +156,8 @@ const BrewRenderer = createClass({
 					onMouseOver={this.prepareScroll}
 					onMouseOut={this.unprepareScroll}
 					ref='main'
-					style={{ height: 	 this.state.height,
-							 willChange: this.state.willChange }}>
+					style={{ height     : this.state.height,
+							 willChange : this.state.willChange }}>
 
 					<ErrorBar errors={this.props.errors} />
 					<RenderWarnings />
