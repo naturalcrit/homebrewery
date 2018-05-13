300e2f0e78fd4e5d847b65e0c1b9afc2ede8ec43
Trevor Buckner <calculuschild@gmail.com>
2018-05-13 09:23:03 -0400

    Move will-change to stylesheet
    This makes `will-change: transform` permanent in the brewRenderer stylesheet rather than trying to load and unload it programatically.

--- client/homebrew/brewRenderer/brewRenderer.jsx
+++ client/homebrew/brewRenderer/brewRenderer.jsx
@@ -25,7 +25,6 @@ const BrewRenderer = createClass({
 		return {
 			viewablePageNumber : 0,
 			height             : 0,
-			willChange         : 'auto',
 			isMounted          : false,
 
 			usePPR : true,
@@ -134,20 +133,6 @@ const BrewRenderer = createClass({
 		return this.lastRender;
 	},
 
-	/**
-	* Optimize for smooth scrolling when mouse enters the rendering panel
-	**/
-	prepareScroll : function(){
-		this.setState({ willChange: 'transform' });
-	},
-
-	/**
-	* Unload smooth scrolling optimizations when mouse leaves rendering panel
-	**/
-	unprepareScroll : function(){
-		this.setState({ willChange: 'auto' });
-	},
-
 	render : function(){
 		return (
 			<React.Fragment>
@@ -156,8 +141,7 @@ const BrewRenderer = createClass({
 					onMouseOver={this.prepareScroll}
 					onMouseOut={this.unprepareScroll}
 					ref='main'
-					style={{ height     : this.state.height,
-							 willChange : this.state.willChange }}>
+					style={{ height : this.state.height }}>
 
 					<ErrorBar errors={this.props.errors} />
 					<RenderWarnings />
