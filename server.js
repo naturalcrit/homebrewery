77e92aa5423b2d7394750e6f05b99963d8fd5d6c
Rae Che <RaeChe@agoa.org.uk>
2018-05-27 12:12:21 +0100

    Remove redundant expression.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const jwt = require('jwt-simple');
 const express = require('express');
 const app = express();
 
-app.use(express.static(`${__dirname}/build`));'';
+app.use(express.static(`${__dirname}/build`));
 app.use(require('body-parser').json({ limit: '25mb' }));
 app.use(require('cookie-parser')());
 
