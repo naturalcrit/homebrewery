5b05440e66c8547c01f2962c4d207250bd80c248
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2018-11-26 19:51:09 -0500

    Adding some more text to the default issue title to guide peopel to add an issue title

--- client/homebrew/navbar/issue.navitem.jsx
+++ client/homebrew/navbar/issue.navitem.jsx
@@ -3,7 +3,11 @@ const createClass = require('create-react-class');
 const Nav = require('naturalcrit/nav/nav.jsx');
 
 module.exports = function(props){
-	return <Nav.item newTab={true} href='https://www.reddit.com/r/homebrewery/submit?selftext=true&title=[Issue]' color='red' icon='fa-bug'>
+	return <Nav.item
+		newTab={true}
+		color='red'
+		icon='fa-bug'
+		href={`https://www.reddit.com/r/homebrewery/submit?selftext=true&title=${encodeURIComponent('[Issue] Describe Your Issue Here')}`} >
 		report issue
 	</Nav.item>;
 };
