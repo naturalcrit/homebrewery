1184fe86a5ebea6967cdfaaf376d9253e2098a24
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2018-12-04 11:27:25 -0500

    Removing the admin key, switching to full basic auth

--- client/admin/brewCleanup/brewCleanup.jsx
+++ client/admin/brewCleanup/brewCleanup.jsx
@@ -2,12 +2,42 @@ const React       = require('react');
 const createClass = require('create-react-class');
 const cx          = require('classnames');
 
+const request = require('superagent');
+
 
 const BrewCleanup = createClass({
 	displayName : 'BrewCleanup',
 	getDefaultProps(){
 		return {
+			adminKey : '',
 		};
+	},
+	getInitialState() {
+		return {
+			count : 0,
+
+			pending : false,
+			primed : false
+		};
+	},
+	prime(){
+		if(this.state.primed) return this.cleanup();
+		this.setState({ pending: true });
+
+		request.get('/admin/cleanup')
+			.query({ admin_key: this.props.adminKey })
+			.then((res)=> this.setState({count : res.body.count }))
+			.catch((err)=>this.setState({ error : err }))
+			.finally(()=>this.setState({ pending : false }))
+	},
+	cleanup(){
+		this.setState({ pending: true });
+
+		request.post('/admin/cleanup')
+			.query({ admin_key: this.props.adminKey })
+			.then((res)=> this.setState({count : res.body.count }))
+			.catch((err)=>this.setState({ error : err }))
+			.finally(()=>this.setState({ pending : false, primed : false }))
 	},
 	render(){
 		return <div className='BrewCleanup'>
