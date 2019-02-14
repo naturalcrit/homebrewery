b678792d4b7b8e1cf7369cb1ec879854d6079bcc
Trevor Buckner <calculuschild@gmail.com>
2019-02-14 13:53:04 -0500

    Lint

--- client/homebrew/pages/userPage/brewItem/brewItem.jsx
+++ client/homebrew/pages/userPage/brewItem/brewItem.jsx
@@ -21,12 +21,11 @@ const BrewItem = createClass({
 		if(this.props.brew.authors.length <= 1){
 			if(!confirm('Are you sure you want to delete this brew? Because you are the only owner of this brew, the document will be deleted permanently.')) return;
 			if(!confirm('Are you REALLY sure? You will not be able to recover the document.')) return;
-		}
-		else{
+		} else {
 			if(!confirm('Are you sure you want to remove this brew from your collection? This will remove you as an editor, but other owners will still be able to access the document.')) return;
 			if(!confirm('Are you REALLY sure? You will lose editor access to this document.')) return;
 		}
-		
+
 		request.get(`/api/remove/${this.props.brew.editId}`)
 			.send()
 			.end(function(err, res){
