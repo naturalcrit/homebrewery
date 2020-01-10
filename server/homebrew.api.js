486841084f78a44fa73f40060fa7cc653ae9d4dd
Trevor Buckner <calculuschild@gmail.com>
2020-01-10 08:59:39 -0500

    Make compression raw (no wrapper). Otherwise small files are actually larger after compression.

--- server/homebrew.api.js
+++ server/homebrew.api.js
@@ -48,7 +48,7 @@ router.put('/api/update/:id', (req, res)=>{
 	HomebrewModel.get({ editId: req.params.id })
 		.then((brew)=>{
 			brew = _.merge(brew, req.body);
-			brew.textBin = zlib.deflateSync(req.body.text);		// Compress brew text to binary before saving
+			brew.textBin = zlib.deflateRawSync(req.body.text);		// Compress brew text to binary before saving
 			brew.text = '';										// Clear out the non-binary text field so its not saved twice
 			brew.updatedAt = new Date();
 
