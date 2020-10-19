1d50cbf684971f5581fbbff3c62c2b23ff589769
Trevor Buckner <calculuschild@gmail.com>
2020-10-19 16:24:19 -0400

    Update admin.api.js

--- server/admin.api.js
+++ server/admin.api.js
@@ -37,7 +37,7 @@ const junkBrewQuery = HomebrewModel.find({
 
 /* Search for brews that aren't compressed (missing the compressed text field) */
 const uncompressedBrewQuery = HomebrewModel.find({
-	'textBin' : null
+	'text' :  { '$exists' : true }
 }).lean().limit(1000).select('_id');
 
 router.get('/admin/cleanup', mw.adminOnly, (req, res)=>{
