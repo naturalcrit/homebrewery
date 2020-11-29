b5602a43d867d68ca1cfaf0fbe8b2abf53a8bc7c
Trevor Buckner <calculuschild@gmail.com>
2020-11-28 20:05:27 -0500

    Mustache-style div blocks

--- shared/naturalcrit/markdown.js
+++ shared/naturalcrit/markdown.js
@@ -13,6 +13,31 @@ renderer.html = function (html) {
 	return html;
 };
 
+// Mustache-style Divs {{class \n content ... \n}}
+let blockCount = 0;
+renderer.paragraph = function(text){
+	const blockReg = /{{[\w|,]+|}}/g;
+	const matches = text.match(blockReg);
+	if(!matches) return `\n<p>${text}</p>\n`;
+	let matchIndex = 0;
+	const res =  _.reduce(text.split(blockReg), (r, text)=>{
+		if(text) r.push(Markdown(text, { renderer: renderer, sanitize: true }));
+
+		const block = matches[matchIndex];
+		if(block && block[0] == '{'){
+			r.push(`\n\n<div class="block ${block.substring(2).split(',').join(' ')}">`);
+			blockCount++;
+		}
+		if(block == '}}' && blockCount !== 0){
+			r.push('</div>\n\n');
+			blockCount--;
+		}
+		matchIndex++;
+		return r;
+	}, []).join('\n');
+	return res;
+};
+
 renderer.link = function (href, title, text) {
 	let self = false;
 	if(href[0] == '#') {
