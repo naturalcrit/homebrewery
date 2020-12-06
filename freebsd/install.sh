65f4094b5aaf08a61f2329fc061744a9f14f4f06
G.Ambatte <sean@robertson-family.nz>
2020-12-06 18:06:23 +1300

    Create install.sh

---
+++ freebsd/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+pkg install -y git nano node npm mongodb36-3.6.20
+
+sysrc mongod_enable=YES
+service mongod start
+
+cd /
+git clone https://github.com/G-Ambatte/homebrewery.git
+
+cd homebrewery
+npm install
+npm audit fix -force
+npm run postinstall
+
+cp freebsd/rc.d/homebrewery /etc/rc.d/
+chmod +x /etc/rc.d/homebrewery
+
+sysrc homebrewery=YES
+service homebrewery start
