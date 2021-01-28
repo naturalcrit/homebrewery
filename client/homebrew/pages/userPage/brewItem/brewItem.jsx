06bb8349c546bbd77ce1cd7f20e2fab259736402
G.Ambatte <sean@robertson-family.nz>
2021-01-28 21:52:36 +1300

    Shows the brew's tagged systems on the User Page summary

--- client/homebrew/pages/userPage/brewItem/brewItem.jsx
+++ client/homebrew/pages/userPage/brewItem/brewItem.jsx
@@ -88,9 +88,10 @@ const BrewItem = createClass({
 
 	render : function(){
 		const brew = this.props.brew;
+		
 		return <div className='brewItem'>
 			<h2>{brew.title}</h2>
-			<h5>Systems: {brew.systems}</h5>
+			<h5>Systems:- {this.props.brew.systems.toString()}</h5>
 			<p className='description' >{brew.description}</p>
 			<hr />
 
