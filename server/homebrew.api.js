93cd93311740cba689a2c8bd75cc961ceaf80642
G.Ambatte <sean@robertson-family.nz>
2021-02-21 13:07:47 +1300

    Reverse unnecessary change that was incorrectly included in previous commit.

--- server/homebrew.api.js
+++ server/homebrew.api.js
@@ -128,6 +128,8 @@ const newGoogleBrew = async (req, res, next)=>{
 
 	req.body = brew;
 
+	console.log(oAuth2Client);
+
 	const newBrew = await GoogleActions.newGoogleBrew(oAuth2Client, brew);
 
 	return res.status(200).send(newBrew);
