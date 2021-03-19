712824d8a646345a4210a844d03a058f06f87aba
G.Ambatte <sean@robertson-family.nz>
2021-03-19 17:44:07 +1300

    Add 'Tweak Drop Cap' with default settings to allow users to tweak the drop cap as desired

--- client/homebrew/editor/snippetbar/snippets/snippets.js
+++ client/homebrew/editor/snippetbar/snippets/snippets.js
@@ -86,6 +86,20 @@ module.exports = [
 						'  }\n' +
 						'</style>'
 			},
+			{
+				name : 'Tweak Drop Cap',
+				icon : 'fas fa-sliders-h',
+				gen  : '<style>\n' +
+						'  /* Drop Cap settings */\n' +
+						'  .phb h1 + p::first-letter {\n' +
+						'    float: left;\n' +
+						'    font-family: Solberry;\n' +
+						'    font-size: 10em;\n' +
+						'    color: #222;\n' +
+						'    line-height: .8em;\n' +
+						'  }\n' +
+						'</style>'
+			},
 		]
 	},
 
