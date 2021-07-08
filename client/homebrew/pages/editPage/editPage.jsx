188720485a144ab06188bb9ea15350e2e0f4e6a4
G.Ambatte <sean@robertson-family.nz>
2021-07-08 20:41:09 +1200

    Update client/homebrew/pages/editPage/editPage.jsx
    Co-authored-by: Trevor Buckner <calculuschild@gmail.com>

--- client/homebrew/pages/editPage/editPage.jsx
+++ client/homebrew/pages/editPage/editPage.jsx
@@ -78,7 +78,7 @@ const EditPage = createClass({
 
 		if(this.isNew()) {
 			console.log('is new');
-			if((!brew.text && localStorage.getItem(BREWKEY)) && (!brew.style && localStorage.getItem(STYLEKEY))){
+			if(!brew.text || !brew.style){
 				console.log('has local data');
 
 				const brewStorage  = localStorage.getItem(BREWKEY) ?? '';
