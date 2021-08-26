4971c40e2322ef5a39c96ab47e5c57bfa6b888bd
G.Ambatte <sean@robertson-family.nz>
2021-08-26 17:21:45 +1200

    Remove undesired properties from brews prior to updating Clean up tab/spacing in `googleActions.js`

--- server/homebrew.api.js
+++ server/homebrew.api.js
@@ -61,10 +61,20 @@ const newBrew = (req, res)=>{
 	});
 };
 
+const cleanBrew = (brew)=>{
+	// Remove undesired properties
+	const undesiredPropertyNames=['views', 'lastViewed'];
+	for (const undesiredProperty of undesiredPropertyNames) {
+		delete brew[undesiredProperty];
+	};
+	return brew;
+};
+
 const updateBrew = (req, res)=>{
 	HomebrewModel.get({ editId: req.params.id })
 		.then((brew)=>{
-			brew = _.merge(brew, req.body);
+			const updateBrew = cleanBrew(req.body);
+			brew = _.merge(brew, updateBrew);
 			brew.text = mergeBrewText(brew.text, brew.style);
 
 			// Compress brew text to binary before saving
@@ -154,7 +164,7 @@ const updateGoogleBrew = async (req, res, next)=>{
 
 	try {	oAuth2Client = GoogleActions.authCheck(req.account, res); } catch (err) { return res.status(err.status).send(err.message); }
 
-	const brew = req.body;
+	const brew = cleanBrew(req.body);
 	brew.text = mergeBrewText(brew.text, brew.style);
 
 	try {
