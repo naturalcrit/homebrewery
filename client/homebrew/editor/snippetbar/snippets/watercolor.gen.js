59fd2454a45151b3e1dbdae182154f6979202052
Trevor Buckner <calculuschild@gmail.com>
2021-08-31 23:57:08 -0400

    Make images local, change snippet to allow color instead of hue-rotate

--- client/homebrew/editor/snippetbar/snippets/watercolor.gen.js
+++ client/homebrew/editor/snippetbar/snippets/watercolor.gen.js
@@ -1,20 +1,5 @@
 const _ = require('lodash');
 
-const watercolorBG = [
-	'https://i.imgur.com/UPUX4zG.png',
-	'https://i.imgur.com/Q6Uquv9.png',
-	'https://i.imgur.com/UOtaJpJ.png',
-	'https://i.imgur.com/Wy2DVk3.png',
-	'https://i.imgur.com/IempVlg.png',
-	'https://i.imgur.com/iJ1ddgd.png',
-	'https://i.imgur.com/qg4an04.png',
-	'https://i.imgur.com/ogGTcCh.png',
-	'https://i.imgur.com/itBD19A.png',
-	'https://i.imgur.com/tbbycAt.png',
-	'https://i.imgur.com/1SVWpGR.png',
-	'https://i.imgur.com/wC1zkuJ.png'
-];
-
 module.exports = ()=>{
-	return `\n![watercolor bg](${_.sample(watercolorBG)}) {watercolor,position:absolute,top:0px,left:0px,width:240px,filter:hue-rotate(0deg),mix-blend-mode:multiply,opacity:80%}\n`;
+	return `{{watercolor${_.random(1, 12)},top:20px,left:30px,width:300px,background-color:#BBAD82,opacity:80%}}\n\n`;
 };
