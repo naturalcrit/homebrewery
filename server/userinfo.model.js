9dfed6bd46efdbde46b706036057a6c4f529eb54
G.Ambatte <sean@robertson-family.nz>
2021-09-12 21:34:03 +1200

    Correct error message

--- server/userinfo.model.js
+++ server/userinfo.model.js
@@ -20,7 +20,7 @@ UserInfoSchema.statics.updateUserActivity = async function(username) {
 UserInfoSchema.statics.getUserInfo = function(username){
 	return new Promise((resolve, reject)=>{
 		UserInfo.find({ username: username }, (err, users)=>{
-			if(err || !users.length) return reject('Can not find brew');
+			if(err || !users.length) return reject('Can not find user');
 			return resolve(users[0]);
 		});
 	});
