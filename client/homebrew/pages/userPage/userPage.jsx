700f84adec40fce24b0c15555ddd4a478fd30d99
Gazook89 <58999374+Gazook89@users.noreply.github.com>
2021-10-04 18:50:52 -0500

    add import issue.navitem.jsx to user page

--- client/homebrew/pages/userPage/userPage.jsx
+++ client/homebrew/pages/userPage/userPage.jsx
@@ -13,6 +13,7 @@ const RecentNavItem = require('../../navbar/recent.navitem.jsx').both;
 const Account = require('../../navbar/account.navitem.jsx');
 const NewBrew = require('../../navbar/newbrew.navitem.jsx');
 const BrewItem = require('./brewItem/brewItem.jsx');
+const ReportIssue = require('../../navbar/issue.navitem.jsx');
 
 // const brew = {
 // 	title   : 'SUPER Long title woah now',
