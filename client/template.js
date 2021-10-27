3ce1ea610dc57f4955801859fa87a28305502b85
G.Ambatte <sean@robertson-family.nz>
2021-10-27 19:51:52 +1300

    Update `template.js` to add <meta> tags

--- client/template.js
+++ client/template.js
@@ -7,6 +7,12 @@ module.exports = async(name, title = '', props = {})=>{
 		<link href="//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700" rel="stylesheet" type="text/css" />
 		<link href=${`/${name}/bundle.css`} rel='stylesheet' />
 		<link rel="icon" href="/assets/homebrew/favicon.ico" type="image/x-icon" />
+		<meta property="og:title" content="${props.brew?.title ? props.brew.title : 'Homebrewery - Untitled Brew'}">
+		<meta property="og:url" content="https://homebrewery.naturalcrit.com${props.brew?.shareId ? `/share/${props.brew.shareId}` : '/new'}">
+		<meta property="og:image" content="${props.brew?.thumbnail ? props.brew.thumbnail : 'https://i.imgur.com/FwRuhv7.png'}">
+		<meta property="og:description" content="${props.brew?.description ? props.brew.description : 'No description.'}">
+		<meta property="og:site_name" content="The Homebrewery - Make your Homebrew content look legit!">
+		<meta property="og:type" content="website">
 		<title>${title.length ? `${title} - The Homebrewery`: 'The Homebrewery - NaturalCrit'}</title>
 	</head>
 	<body>
