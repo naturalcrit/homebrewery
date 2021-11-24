9618e802d1c602bcb7919ded545d9e49cfac587c
Gazook89 <58999374+Gazook89@users.noreply.github.com>
2021-11-24 14:35:36 -0600

    Update client/homebrew/pages/editPage/editPage.jsx
    Co-authored-by: Trevor Buckner <calculuschild@gmail.com>

--- client/homebrew/pages/editPage/editPage.jsx
+++ client/homebrew/pages/editPage/editPage.jsx
@@ -349,7 +349,7 @@ const EditPage = createClass({
 				</Nav.item>;
 			}
 
-			if(this.state.errors.url.match(/^\/api\/.*Google.*$/m)){
+			if(this.state.errors.response.req.url.match(/^\/api\/.*Google.*$/m)){
 				return <Nav.item className='save error' icon='fas fa-exclamation-triangle'>
 					Oops!
 					<div className='errorContainer' onClick={this.clearErrors}>
