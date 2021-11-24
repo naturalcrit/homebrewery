2e5d1b3b5572c95a5b2945fb2e2eb5f957c80175
Gazook89 <58999374+Gazook89@users.noreply.github.com>
2021-11-24 07:48:44 -0600

    Update client/homebrew/pages/newPage/newPage.jsx
    Co-authored-by: Trevor Buckner <calculuschild@gmail.com>

--- client/homebrew/pages/newPage/newPage.jsx
+++ client/homebrew/pages/newPage/newPage.jsx
@@ -226,7 +226,7 @@ const NewPage = createClass({
 				</Nav.item>;
 			}
 
-			if(this.state.errors.url.match(/^\/api\/.*Google.*$/m)){
+			if(this.state.errors.response.req.url.match(/^\/api\/.*Google.*$/m)){
 				return <Nav.item className='save error' icon='fas fa-exclamation-triangle'>
 					Oops!
 					<div className='errorContainer' onClick={this.clearErrors}>
