46856d011919194f9b90dab84adf2d73e41f2aea
Charlie Humphreys <ahumphreys3@gmail.com>
2021-12-20 08:16:16 -0600

    Fix linting

--- client/homebrew/navbar/discord.navitem.jsx
+++ client/homebrew/navbar/discord.navitem.jsx
@@ -2,12 +2,12 @@ const React = require('react');
 const Nav = require('naturalcrit/nav/nav.jsx');
 
 module.exports = function(props){
-    return <Nav.item
-        className='discord'
-        newTab={true}
-        href='https://discord.gg/by3deKx'
-        color='blurple'
-        icon='fab fa-discord'>
+	return <Nav.item
+		className='discord'
+		newTab={true}
+		href='https://discord.gg/by3deKx'
+		color='blurple'
+		icon='fab fa-discord'>
         discord
-    </Nav.item>;
+	</Nav.item>;
 };
