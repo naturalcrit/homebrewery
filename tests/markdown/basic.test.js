28793e06fcfa494bcb1e59ebff874d26b74a6c2e
Alexey Sachkov <sachkov2011@gmail.com>
2021-12-30 23:40:58 +0300

    Fix linter errors

--- tests/markdown/basic.test.js
+++ tests/markdown/basic.test.js
@@ -3,13 +3,13 @@
 const Markdown = require('naturalcrit/markdown.js');
 
 test('Escapes <script> tag', function() {
-  const source = '<script></script>';
-  const rendered = Markdown.render(source);
-  expect(rendered).toMatch('&lt;script&gt;&lt;/script&gt;');
+	const source = '<script></script>';
+	const rendered = Markdown.render(source);
+	expect(rendered).toMatch('&lt;script&gt;&lt;/script&gt;');
 });
 
 test('Processes the markdown within an HTML block if its just a class wrapper', function() {
-  const source = '<div>*Bold text*</div>';
-  const rendered = Markdown.render(source);
-  expect(rendered).toBe('<div> <p><em>Bold text</em></p>\n </div>');
+	const source = '<div>*Bold text*</div>';
+	const rendered = Markdown.render(source);
+	expect(rendered).toBe('<div> <p><em>Bold text</em></p>\n </div>');
 });
--- tests/markdown/mustache-span.test.js
+++ tests/markdown/mustache-span.test.js
@@ -3,106 +3,106 @@
 const Markdown = require('naturalcrit/markdown.js');
 
 test('Renders a mustache span with text only', function() {
-    const source = '{{ text}}';
-    const rendered = Markdown.render(source);
-    expect(rendered).toBe('<span class="inline-block ">text</span>');
+	const source = '{{ text}}';
+	const rendered = Markdown.render(source);
+	expect(rendered).toBe('<span class="inline-block ">text</span>');
 });
 
 test('Renders a mustache span with text only, but with spaces', function() {
-    const source = '{{ this is a text}}';
-    const rendered = Markdown.render(source);
-    expect(rendered).toBe('<span class="inline-block ">this is a text</span>');
+	const source = '{{ this is a text}}';
+	const rendered = Markdown.render(source);
+	expect(rendered).toBe('<span class="inline-block ">this is a text</span>');
 });
 
 test('Renders an empty mustache span', function() {
-    const source = '{{}}';
-    const rendered = Markdown.render(source);
-    expect(rendered).toBe('<span class="inline-block "></span>');
+	const source = '{{}}';
+	const rendered = Markdown.render(source);
+	expect(rendered).toBe('<span class="inline-block "></span>');
 });
 
 test('Renders a mustache span with just a space', function() {
-    const source = '{{ }}';
-    const rendered = Markdown.render(source);
-    expect(rendered).toBe('<span class="inline-block "></span>');
+	const source = '{{ }}';
+	const rendered = Markdown.render(source);
+	expect(rendered).toBe('<span class="inline-block "></span>');
 });
 
 test('Renders a mustache span with a few spaces only', function() {
-    const source = '{{     }}';
-    const rendered = Markdown.render(source);
-    expect(rendered).toBe('<span class="inline-block "></span>');
+	const source = '{{     }}';
+	const rendered = Markdown.render(source);
+	expect(rendered).toBe('<span class="inline-block "></span>');
 });
 
 test('Renders a mustache span with text and class', function() {
-    const source = '{{my-class text}}';
-    const rendered = Markdown.render(source);
-    // FIXME: why do we have those two extra spaces after closing "?
-    expect(rendered).toBe('<span class="inline-block my-class"  >text</span>');
+	const source = '{{my-class text}}';
+	const rendered = Markdown.render(source);
+	// FIXME: why do we have those two extra spaces after closing "?
+	expect(rendered).toBe('<span class="inline-block my-class"  >text</span>');
 });
 
 test('Renders a mustache span with text and two classes', function() {
-    const source = '{{my-class,my-class2 text}}';
-    const rendered = Markdown.render(source);
-    // FIXME: why do we have those two extra spaces after closing "?
-    expect(rendered).toBe('<span class="inline-block my-class my-class2"  >text</span>');
+	const source = '{{my-class,my-class2 text}}';
+	const rendered = Markdown.render(source);
+	// FIXME: why do we have those two extra spaces after closing "?
+	expect(rendered).toBe('<span class="inline-block my-class my-class2"  >text</span>');
 });
 
 test('Renders a mustache span with text with spaces and class', function() {
-    const source = '{{my-class this is a text}}';
-    const rendered = Markdown.render(source);
-    // FIXME: why do we have those two extra spaces after closing "?
-    expect(rendered).toBe('<span class="inline-block my-class"  >this is a text</span>');
+	const source = '{{my-class this is a text}}';
+	const rendered = Markdown.render(source);
+	// FIXME: why do we have those two extra spaces after closing "?
+	expect(rendered).toBe('<span class="inline-block my-class"  >this is a text</span>');
 });
 
 test('Renders a mustache span with text and id', function() {
-    const source = '{{#my-span text}}';
-    const rendered = Markdown.render(source);
-    // FIXME: why do we have that one extra space after closing "?
-    expect(rendered).toBe('<span class="inline-block " id="my-span" >text</span>');
+	const source = '{{#my-span text}}';
+	const rendered = Markdown.render(source);
+	// FIXME: why do we have that one extra space after closing "?
+	expect(rendered).toBe('<span class="inline-block " id="my-span" >text</span>');
 });
 
 test('Renders a mustache span with text and two ids', function() {
-    const source = '{{#my-span,#my-favorite-span text}}';
-    const rendered = Markdown.render(source);
-    // FIXME: do we need to report an error here somehow?
-    expect(rendered).toBe('<span class="inline-block " id="my-span" >text</span>');
+	const source = '{{#my-span,#my-favorite-span text}}';
+	const rendered = Markdown.render(source);
+	// FIXME: do we need to report an error here somehow?
+	expect(rendered).toBe('<span class="inline-block " id="my-span" >text</span>');
 });
 
 test('Renders a mustache span with text and css property', function() {
-    const source = '{{color:red text}}';
-    const rendered = Markdown.render(source);
-    expect(rendered).toBe('<span class="inline-block "  style="color:red;">text</span>');
+	const source = '{{color:red text}}';
+	const rendered = Markdown.render(source);
+	expect(rendered).toBe('<span class="inline-block "  style="color:red;">text</span>');
 });
 
 test('Renders a mustache span with text and two css properties', function() {
-    const source = '{{color:red,padding:5px text}}';
-    const rendered = Markdown.render(source);
-    expect(rendered).toBe('<span class="inline-block "  style="color:red; padding:5px;">text</span>');
+	const source = '{{color:red,padding:5px text}}';
+	const rendered = Markdown.render(source);
+	expect(rendered).toBe('<span class="inline-block "  style="color:red; padding:5px;">text</span>');
 });
 
 test('Renders a mustache span with text and css property which contains quotes', function() {
-    const source = '{{font:"trebuchet ms" text}}';
-    const rendered = Markdown.render(source);
-    // FIXME: is it correct to remove quotes surrounding css property value?
-    expect(rendered).toBe('<span class="inline-block "  style="font:trebuchet ms;">text</span>');
+	const source = '{{font:"trebuchet ms" text}}';
+	const rendered = Markdown.render(source);
+	// FIXME: is it correct to remove quotes surrounding css property value?
+	expect(rendered).toBe('<span class="inline-block "  style="font:trebuchet ms;">text</span>');
 });
 
 test('Renders a mustache span with text and two css properties which contains quotes', function() {
-    const source = '{{font:"trebuchet ms",padding:"5px 10px" text}}';
-    const rendered = Markdown.render(source);
-    expect(rendered).toBe('<span class="inline-block "  style="font:trebuchet ms; padding:5px 10px;">text</span>');
+	const source = '{{font:"trebuchet ms",padding:"5px 10px" text}}';
+	const rendered = Markdown.render(source);
+	expect(rendered).toBe('<span class="inline-block "  style="font:trebuchet ms; padding:5px 10px;">text</span>');
 });
 
 
 test('Renders a mustache span with text with quotes and css property which contains quotes', function() {
-    const source = '{{font:"trebuchet ms" text "with quotes"}}';
-    const rendered = Markdown.render(source);
-    expect(rendered).toBe('<span class="inline-block "  style="font:trebuchet ms;">text “with quotes”</span>');
+	const source = '{{font:"trebuchet ms" text "with quotes"}}';
+	const rendered = Markdown.render(source);
+	expect(rendered).toBe('<span class="inline-block "  style="font:trebuchet ms;">text “with quotes”</span>');
 });
 
 test('Renders a mustache span with text, id, class and a couple of css properties', function() {
-    const source = '{{pen,#author,color:orange,font-family:"trebuchet ms" text}}';
-    const rendered = Markdown.render(source);
-    expect(rendered).toBe('<span class="inline-block pen" id="author" style="color:orange; font-family:trebuchet ms;">text</span>');
+	const source = '{{pen,#author,color:orange,font-family:"trebuchet ms" text}}';
+	const rendered = Markdown.render(source);
+	expect(rendered).toBe('<span class="inline-block pen" id="author" style="color:orange; font-family:trebuchet ms;">text</span>');
 });
 
 // TODO: add tests for ID with accordance to CSS spec:
