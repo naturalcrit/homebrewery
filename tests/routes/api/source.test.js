35c65cf746d4144f5085b72220b3ff1b524ad03c
Alexey Sachkov <sachkov2011@gmail.com>
2022-01-23 23:22:01 +0300

    Add tests for source/ route

---
+++ tests/routes/api/source.test.js
@@ -0,0 +1,92 @@
+const supertest = require('supertest');
+
+// Mimic https responses to avoid being redirected all the time
+const app = supertest.agent(require('app.js').app)
+    .set('X-Forwarded-Proto', 'https');
+
+const DB = require('db.js');
+const Homebrew = require('homebrew.model.js');
+
+// FIXME: outline config creation into a separate module
+const config = require('nconf')
+	.argv()
+	.env({ lowerCase: true })
+	.file('environment', { file: `config/${process.env.NODE_ENV}.json` })
+	.file('defaults', { file: 'config/default.json' });
+
+describe('/source/:id', ()=>{
+	beforeAll(()=>{
+		return DB.connect(config);
+	});
+
+	beforeAll(async ()=>{
+		const regular_brew = new Homebrew.model({
+			shareId : 'share-id-1',
+			text    : 'This is text',
+			authors : ['this', 'is', 'list', 'of', 'authors']
+		});
+		await regular_brew.save();
+        
+        const brew_with_special_symbols = new Homebrew.model({
+            shareId : 'share-id-2',
+			text    : '<div>&</div>'
+        });
+        await brew_with_special_symbols.save();
+	});
+
+	it('able to return a source of an existing brew', ()=>{
+		return app.get('/source/share-id-1')
+            .send()
+            .expect(200)
+            .then((response)=>{
+                expect(response.text).toBe('<code><pre style="white-space: pre-wrap;">This is text</pre></code>');
+                expect(response.headers).toHaveProperty('content-type', 'text/html; charset=utf-8');
+            });
+	});
+
+    it('encodes special symbols', ()=>{
+        return app.get('/source/share-id-2')
+            .send()
+            .expect(200)
+            .then((response)=>{
+                expect(response.text).toBe('<code><pre style="white-space: pre-wrap;">&lt;div&gt;&amp;&lt;/div&gt;</pre></code>');
+                expect(response.headers).toHaveProperty('content-type', 'text/html; charset=utf-8');
+            });
+    });
+
+    it('returns an error for a non-existing brew', ()=>{
+        return app.get('/source/invalid-id')
+            .send()
+            // FIXME: we should be expecting 404 Not Found (#1983)
+            .expect(500);
+    });
+
+    it('returns an error for a non-existing brew (google id)', ()=>{
+        return app.get('/source/non-existing-brew-id')
+            .send()
+            // FIXME: we should be expecting 404 Not Found (#1983)
+            .expect(500);
+    });
+
+    // FIXME: we should return an error instead of a home page here
+    it.skip('returns an error for a missing brew id', ()=>{
+        return app.get('/source/')
+            .send()
+            .expect(404);
+    });
+
+    // FIXME: we should return an error instead of a home page here
+    it.skip('returns an error for a missing brew id #2', ()=>{
+        return app.get('/source')
+            .send()
+            .expect(404);
+    });
+
+	afterAll(()=>{
+		return Homebrew.model.deleteMany();
+	});
+
+	afterAll(()=>{
+		return DB.disconnect();
+	});
+});
