af950159e0588b5a48916f4ec7f9819876474ae2
G.Ambatte <sean@robertson-family.nz>
2022-04-13 08:24:25 +1200

    Update client/template.js
    Co-authored-by: Trevor Buckner <calculuschild@gmail.com>

--- client/template.js
+++ client/template.js
@@ -9,7 +9,7 @@ module.exports = async(name, title = '', props = {})=>{
 		<link href="//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700" rel="stylesheet" type="text/css" />
 		<link href=${`/${name}/bundle.css`} rel='stylesheet' />
 		<link rel="icon" href="/assets/homebrew/favicon.ico" type="image/x-icon" />
-		<meta property="og:title" content="${props.brew?.title ? props.brew.title : 'Homebrewery - Untitled Brew'}">
+		<meta property="og:title" content="${props.brew?.title ?? 'Homebrewery - Untitled Brew'}">
 		<meta property="og:url" content="${HOMEBREWERY_PUBLIC_URL}/${props.brew?.shareId ? `share/${props.brew.shareId}` : ''}">
 		<meta property="og:image" content="${props.brew?.thumbnail ?? `${HOMEBREWERY_PUBLIC_URL}/thumbnail.png`}">
 		<meta property="og:description" content="${props.brew?.description ? props.brew.description : 'No description.'}">
