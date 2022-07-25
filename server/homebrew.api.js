ca9ec2ec6b1f34342dfe48c3e5a276fada27e6be
Josef Utbult <josef@utbult.design>
2022-07-25 22:29:40 +0200

    Add functionality to get a list of available themes, themes stylesheets and running snippets from a theme in the API

--- server/homebrew.api.js
+++ server/homebrew.api.js
@@ -5,6 +5,8 @@ const zlib = require('zlib');
 const GoogleActions = require('./googleActions.js');
 const Markdown = require('../shared/naturalcrit/markdown.js');
 const yaml = require('js-yaml');
+const Themes = require('../themes/themes.json');
+const { send } = require('process');
 
 // const getTopBrews = (cb) => {
 // 	HomebrewModel.find().sort({ views: -1 }).limit(5).exec(function(err, brews) {
@@ -182,6 +184,138 @@ const updateGoogleBrew = async (req, res, next)=>{
 	}
 };
 
+// Parse content of themes.json and sends a version containing name and path to all themes.
+// Also tries to import the snippets.js file for the theme and sends names, paths and icon 
+// strings for all available snippet groups and snippets
+const getThemes = (req, res)=>{
+
+	let themesData = {
+		// Homebrewery provided themes that are located in the source code
+		"homebrewery-themes": [],
+		// User made themes (might be implemented in the future)
+		"user-themes": []
+	};
+	Themes["V3"].forEach(theme => {
+		let snippetGroups = [];
+
+		try {
+			const themeSnippets = require("../themes/V3/" + theme["path"] + "/snippets.js");
+			
+			themeSnippets.forEach(group => {
+				snippetsCategory = []
+				group['snippets'].forEach(snippet => {
+					snippetsCategory.push({
+						'name': snippet['name'],
+						'path': encodeURIComponent(snippet['name']),
+						'icon': snippet['icon']
+					});
+				});
+				snippetGroups.push({
+					'groupName': group['groupName'],
+					'path': encodeURIComponent(group['groupName']),
+					'icon': group['icon'],
+					'view': group['view'],
+					'snippets': snippetsCategory
+				});
+			});
+		}
+		catch(error) {
+			if (error.code !== 'MODULE_NOT_FOUND') {
+				throw error;
+			}
+		}
+
+		// TODO: Add user made themes to the same object, using the same format, when that is implemented
+
+		themesData["homebrewery-themes"].push({
+			"name": theme["name"],
+			"path": theme["path"],
+			"snippets": snippetGroups
+		});
+	});
+
+	res.type('json');
+    res.send(themesData);
+};
+
+// Checks for a specific theme from the path in the url and sends its style.css
+const getThemeStyle = (req, res)=>{
+	const path = '/' + req.params.path + '/style.css'	
+	options = {
+		'root': './build/themes/V3',
+		'headers': {
+			'Content-Type': 'text/css'
+		}
+	};
+
+    res.sendFile(path, options, function (err) {
+		if (err) {
+			if (err.code === 'ENOENT') {
+				res.status(404).send("No such file: " + path);
+			}
+            else {
+				res.status(500).send(err);
+			}
+        }
+    });
+};
+
+// Checks for a snippet in a theme, tries to run it and return the results
+const getThemeSnippet = (req, res, next)=>{
+	let snippetOutput = "";
+
+	// Try loading the snippets.js file for the theme
+	try {
+		const themeSnippets = require("../themes/V3/" + req.params.path + "/snippets.js");
+		
+		// Decode the group URL and try to find it in the themes snippet groups
+		const groupName = decodeURIComponent(req.params.snippetGroup);
+		if(!groupName) {
+			return res.status(400).send("Unspecified group name");
+		}
+		const group = themeSnippets.find((instance)=>{return instance['groupName'] == groupName});
+		if(!group) {
+			return res.status(400).send("No such group in " + req.params.path + ": " + groupName);		
+		}
+
+		// Decode the snippet URL and try to find it in the specified snippet group
+		const snippetName = decodeURIComponent(req.params.snippet);
+		if(!snippetName) {
+			return res.status(400).send("Unspecified snippet name");
+		}
+		const snippet = group['snippets'].find((instance)=>{return instance['name'] == snippetName});
+		if(!snippet) {
+			return res.status(400).send("No such snippet in " + req.params.path + ", group " + groupName + ": " + snippetName);
+		}
+
+		// If the snippet is a function, it tries to run it. If it is a string it is passed on as is
+		if(snippet['gen'] instanceof Function) {
+			try {
+				snippetOutput = snippet['gen']();
+			}
+			catch {
+				return res.status(500).send("Malformed snippet (function");
+			}
+		}
+		else if(typeof snippet['gen'] == 'string' || snippet['gen'] instanceof String) {
+			snippetOutput = snippet['gen'];
+		}
+		else {
+			return res.status(500).send("Malformed snippet (undefined)");
+		}
+	}
+	catch(error) {
+		if (error.code !== 'MODULE_NOT_FOUND') {
+			throw error;
+		}
+		return res.status(404).send("No such file: " + req.params.path );
+	}
+
+	// Returns the result from running a snippet
+	return res.send(snippetOutput);
+};
+
+
 router.post('/api', newBrew);
 router.post('/api/newGoogle/', newGoogleBrew);
 router.put('/api/:id', updateBrew);
@@ -190,5 +324,9 @@ router.put('/api/updateGoogle/:id', updateGoogleBrew);
 router.delete('/api/:id', deleteBrew);
 router.get('/api/remove/:id', deleteBrew);
 router.get('/api/removeGoogle/:id', (req, res)=>{GoogleActions.deleteGoogleBrew(req, res, req.params.id);});
+
+router.post('/api/themes/', getThemes);
+router.post('/api/themes/:path/style', getThemeStyle);
+router.post('/api/themes/:path/snippets/:snippetGroup/:snippet', getThemeSnippet);
 
 module.exports = router;
