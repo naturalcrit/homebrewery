b8d7d1a8e484dc6cde60dc7dc945e27f86fe8e12
Trevor Buckner <calculuschild@gmail.com>
2022-09-25 21:43:40 -0400

    Allow rotation and positioning up and down

--- themes/V3/Blank/snippets/imageMask.gen.js
+++ themes/V3/Blank/snippets/imageMask.gen.js
@@ -1,7 +1,7 @@
 const _ = require('lodash');
 
 module.exports = ()=>{
-	return `{{imageMask${_.random(1, 6)},top
-![](https://assets1.ignimgs.com/2019/05/29/dndmobile-br-1559158957902_160w.jpg?width=1280)
+	return `{{imageMask${_.random(1, 6)},--offset:0cm,--rotation:0
+![](https://assets1.ignimgs.com/2019/05/29/dndmobile-br-1559158957902_160w.jpg?width=1280){height:100%}
 }}\n\n`;
 };
