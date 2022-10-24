0b5d9714c02402b71112548e46d96159c198e066
Gazook89 <58999374+Gazook89@users.noreply.github.com>
2022-10-23 22:27:12 -0500

    remove complicated languageCode regexp

--- client/homebrew/editor/metadataEditor/metadataEditor.jsx
+++ client/homebrew/editor/metadataEditor/metadataEditor.jsx
@@ -103,32 +103,6 @@ const MetadataEditor = createClass({
 			});
 	},
 
-	constructLanguageRegExp : function(){
-		const grandfathered = '(' +
-		/* irregular */ '(en-GB-oed|i-ami|i-bnn|i-default|i-enochian|i-hak|i-klingon|i-lux|i-mingo|i-navajo|i-pwn|i-tao|i-tay|i-tsu|sgn-BE-FR|sgn-BE-NL|sgn-CH-DE)' +
-		'|' +
-		/* regular */ '(art-lojban|cel-gaulish|no-bok|no-nyn|zh-guoyu|zh-hakka|zh-min|zh-min-nan|zh-xiang)' +
-		')';
-		const langtag = '(' +
-			'(' + (
-			'([A-Za-z]{2,3}(-' +
-				'([A-Za-z]{3}(-[A-Za-z]{3}){0,2})' +
-				')?)|[A-Za-z]{4}|[A-Za-z]{5,8})'
-		) +
-			'(-' + '([A-Za-z]{4})' + ')?' +
-			'(-' + '([A-Za-z]{2}|[0-9]{3})' + ')?' +
-			'(-' + '([A-Za-z0-9]{5,8}|[0-9][A-Za-z0-9]{3})' + ')*' +
-			'(-' + '(' + (
-			/* singleton */ '[0-9A-WY-Za-wy-z]' +
-				'(-[A-Za-z0-9]{2,8})+)'
-		) +
-			')*' +
-			'(-' + '(x(-[A-Za-z0-9]{1,8})+)' + ')?' +
-			')';
-		return  '^(' + grandfathered + '|' + langtag + '|' + '(x(-[A-Za-z0-9]{1,8})+)' + ')$';
-
-	},
-
 	renderSystems : function(){
 		return _.map(SYSTEMS, (val)=>{
 			return <label key={val}>
