b6c09683beaa8aa7d1297a4590f35809bfe5e080
Gazook89 <58999374+Gazook89@users.noreply.github.com>
2022-11-05 19:11:36 -0500

    add language field validation (for future pr)
    fix an oversight in validations

--- client/homebrew/editor/metadataEditor/validations.js
+++ client/homebrew/editor/metadataEditor/validations.js
@@ -1,17 +1,22 @@
 module.exports = {
 	title : [
 		(value)=>{
-			return value?.length > 10 ? 'Max title length of 10 characters' : null;
+			return value?.length > 100 ? 'Max title length of 100 characters' : null;
 		}
 	],
 	description : [
 		(value)=>{
-			return value?.length > 10 ? 'Max description length of 10 characters' : null;
+			return value?.length > 500 ? 'Max description length of 500 characters.' : null;
 		}
 	],
 	thumbnail : [
 		(value)=>{
-			return value?.length > 5 ? 'Max URL length of 5 characters' : null;
+			return value?.length > 256 ? 'Max URL length of 256 characters.' : null;
+		}
+	],
+	language : [
+		(value)=>{
+			return new RegExp(/[a-z]{2,3}(-.*)?/).test(value || '') === false ? 'Invalid language code.' : null;
 		}
 	]
 };
