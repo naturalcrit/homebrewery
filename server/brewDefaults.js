32506860ddaa838e6dd595d6e4e836ae9a3eb03a
G.Ambatte <sean@robertson-family.nz>
2022-12-14 21:26:57 +1300

    Add additional fields to default

--- server/brewDefaults.js
+++ server/brewDefaults.js
@@ -2,6 +2,9 @@ const _ = require('lodash');
 
 // Default brew properties in most cases
 const DEFAULT_BREW = {
+	text        : '',
+	editId      : null,
+	shareId     : null,
 	title       : 'Untitled Brew',
 	description : '',
 	renderer    : 'V3',
@@ -13,10 +16,13 @@ const DEFAULT_BREW = {
 	theme       : '5ePHB'
 };
 //  Default brew properties for loading
-const DEFAULT_BREW_LOAD = _.defaults({
-	renderer  : 'legacy',
-	published : true
-}, DEFAULT_BREW);
+const DEFAULT_BREW_LOAD = {};
+_.defaults(DEFAULT_BREW_LOAD,
+	{
+		renderer  : 'legacy',
+		published : true
+	},
+	DEFAULT_BREW);
 
 module.exports = {
 	DEFAULT_BREW,
