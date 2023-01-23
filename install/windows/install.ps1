029230e07509464d1bdefa32528135adad4f4096
G.Ambatte <sean@robertson-family.nz>
2023-01-23 15:33:00 +1300

    Initial commit

---
+++ install/windows/install.ps1
@@ -0,0 +1,26 @@
+Write-Host Install Chocolatey
+Write-Host Instructions from https://chocolate.org/install
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install nodejs --version=16.11.1 -y
+
+choco install mongodb --version=4.4.4 -y
+
+choco install git -y
+
+refreshenv
+
+mkdir C:\Hombrewery
+cd C:\Hombrewery
+
+git clone https://github.com/naturalcrit/homebrewery.github
+
+cd homebrewery
+
+npm install
+npm audit fix
+
+[System.Environment]::SetEnvironmentVariable('NODE_ENV', 'local')
+
+npm start
