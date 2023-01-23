c956100416f27ee7af8dc8028f4e0f6b0dab9e01
G.Ambatte <sean@robertson-family.nz>
2023-01-23 18:16:06 +1300

    Improve commenting in script

--- install/windows/install.ps1
+++ install/windows/install.ps1
@@ -3,24 +3,42 @@ Write-Host Instructions from https://chocolate.org/install
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+Write-Host Install Node JS v16.11.1
+
 choco install nodejs --version=16.11.1 -y
+
+Write-Host Install MongoDB v 4.4.4
 
 choco install mongodb --version=4.4.4 -y
 
+Write-Host Install GIT
+
 choco install git -y
 
+Write-Host Refresh Environment
+
 refreshenv
+
+Write-Host Create directory
 
 mkdir C:\Hombrewery
 cd C:\Hombrewery
 
+Write-Host Download Homebrewery project files
+
 git clone https://github.com/naturalcrit/homebrewery.git
+
+Write-Host Install Homebrewery files
 
 cd homebrewery
 
 npm install
 npm audit fix
 
+Write-Host Set install type to 'local'
+
 [System.Environment]::SetEnvironmentVariable('NODE_ENV', 'local')
+
+Write-Host Start Homebrewery
 
 npm start
