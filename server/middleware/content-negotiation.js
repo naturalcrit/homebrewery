b77c70054a4bfad09514d2dd8988689c4b156f72
Charlie Humphreys <ahumphreys3@gmail.com>
2023-01-25 23:00:05 -0600

    adjust content-negotiation middleware to elvis on the accept header

--- server/middleware/content-negotiation.js
+++ server/middleware/content-negotiation.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next)=>{
-	const isImageRequest = req.get('Accept').split(',')
-        .filter((h)=>!h.includes('q='))
-        .every((h)=>/image\/.*/.test(h));
+	const isImageRequest = req.get('Accept')?.split(',')
+        ?.filter((h)=>!h.includes('q='))
+        ?.every((h)=>/image\/.*/.test(h));
 	if(isImageRequest) {
 		return res.status(406).send({
 			message : 'Request for image at this URL is not supported'
