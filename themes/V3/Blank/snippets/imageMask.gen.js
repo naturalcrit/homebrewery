7af2e8810afbd2ff7fdc36b5b58ef7f82c9e1b03
Trevor Buckner <calculuschild@gmail.com>
2023-01-31 16:00:31 -0500

    Fix snippet more, add link to imgur image

--- themes/V3/Blank/snippets/imageMask.gen.js
+++ themes/V3/Blank/snippets/imageMask.gen.js
@@ -5,19 +5,19 @@ module.exports = {
 	edge : ()=>{
 		return dedent`
 			{{imageMask${_.random(1, 8)},--offset:0cm,--rotation:0
-			  ![](https://assets1.ignimgs.com/2019/05/29/dndmobile-br-1559158957902_160w.jpg?width=1280){height:100%}
-
-			  <!-- Use offset to shift the edge up or down from the page center. Use rotation to set rotation angle in degrees. -->
-			}}\n\n`;
+			  ![](https://i.imgur.com/GZfjDWV.png){height:100%}
+			}}
+			<!-- Use --offset to shift the edge up or down from the page center.
+			     Use --rotation to set rotation angle in degrees. -->\n\n`;
 	},
 
 	corner : ()=>{
 		return dedent`
 			{{imageMask_Corner${_.random(1, 8)},--offset:0cm,bottom,left
-			  ![](https://assets1.ignimgs.com/2019/05/29/dndmobile-br-1559158957902_160w.jpg?width=1280){height:100%}
-
-			  <!-- Use offset to shift the edge up or down. Use top, bottom, left, and right to select a corner -->
-			}}\n\n`;
+			  ![](https://i.imgur.com/1w5khYt.png){height:100%}
+			}}
+			<!-- Use offset to shift the edge up or down.
+			     Use top, bottom, left, and right to select a corner -->\n\n`;
 	}
 
 };
