bd4c24df46506ddd41163849b2546108e3c61850
G.Ambatte <sean@robertson-family.nz>
2023-03-15 13:46:30 +1300

    Adjust styling for unusual browser widths

--- client/homebrew/navbar/metadata.navitem.jsx
+++ client/homebrew/navbar/metadata.navitem.jsx
@@ -47,16 +47,26 @@ const MetadataNav = createClass({
 		if(!this.state.showMetaWindow) return null;
 
 		return <div className='dropdown'>
-			<h4>Description</h4>
-			<p>{this.props.brew.description || 'No description.'}</p>
-			<h4>Authors</h4>
-			<p>{this.getAuthors()}</p>
-			<h4>Tags</h4>
-			<p>{this.getTags()}</p>
-			<h4>Systems</h4>
-			<p>{this.getSystems()}</p>
-			<h4>Last Updated</h4>
-			<p>{Moment(this.props.brew.updatedAt).fromNow()}</p>
+			<div className='row'>
+				<h4>Description</h4>
+				<p>{this.props.brew.description || 'No description.'}</p>
+			</div>
+			<div className='row'>
+				<h4>Authors</h4>
+				<p>{this.getAuthors()}</p>
+			</div>
+			<div className='row'>
+				<h4>Tags</h4>
+				<p>{this.getTags()}</p>
+			</div>
+			<div className='row'>
+				<h4>Systems</h4>
+				<p>{this.getSystems()}</p>
+			</div>
+			<div className='row'>
+				<h4>Last Updated</h4>
+				<p>{Moment(this.props.brew.updatedAt).fromNow()}</p>
+			</div>
 		</div>;
 	},
 
