66b9a792e74456a0c62a77f7b9c58a71b442f3d5
Trevor Buckner <calculuschild@gmail.com>
2023-06-24 01:47:07 -0400

    Change Missing Google error text

--- client/homebrew/pages/errorPage/errors/errorIndex.js
+++ client/homebrew/pages/errorPage/errors/errorIndex.js
@@ -13,9 +13,9 @@ const errorIndex = () => {
 				and make sure the Homebrewery folder is still there, and that it holds your brews
 				as text files.
 				- **You may have changed the sharing settings for your files.** If the files
-				are still on Google Drive, change all of them to be shared **with everyone who has
-				the link** so the Homebrewery can access them.
-				- Rarely, it might mean that your Google Account is full or has been closed by
+				are still on Google Drive, change all of them to be shared *with everyone who has
+				the link* so the Homebrewery can access them.
+				- **Your Google Account may be full**, or may be have been closed by
 				Google (for inactivity, violating some policy of theirs). Make sure you can still
 				access your Google Drive normally and upload/download files to it.
 				:
@@ -26,14 +26,14 @@ const errorIndex = () => {
 				deleted and by whom.
 				:
 				If you *still* can't find it, some people have had success asking Google to recover
-				accidentally deleted files. You can visit this link
+				accidentally deleted files at this link: 
 				https://support.google.com/drive/answer/1716222?hl=en&ref_topic=7000946.
-				At the bottom of the page there is a button that says **Send yourself an Email**
+				At the bottom of the page there is a button that says *Send yourself an Email*
 				and you will receive instructions on how to request the files be restored.
 				:
 				Also note, if you prefer not to use your Google Drive for storage, you can always
 				change the storage location of a brew by clicking the Google drive icon by the
-				brew title and choosing **transfer my brew to/from Google Drive**.`
+				brew title and choosing *transfer my brew to/from Google Drive*.`
 	};
 };
 
