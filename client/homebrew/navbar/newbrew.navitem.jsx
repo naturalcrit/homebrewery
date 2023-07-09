4d68d1e4cbd98ef0689edba212fcc5e979965747
Lukas R <mail@sarbot.de>
2023-07-09 21:40:36 +0200

    new/dropdown templates

--- client/homebrew/navbar/newbrew.navitem.jsx
+++ client/homebrew/navbar/newbrew.navitem.jsx
@@ -2,10 +2,38 @@ const React = require('react');
 const Nav = require('naturalcrit/nav/nav.jsx');
 
 module.exports = function(props){
-	return <Nav.item
-		href='/new'
-		color='purple'
+	return <Nav.dropdown>
+		<Nav.item
+			color='purple'
+			className="newNavButton"
+			icon='fas fa-plus-square'>
+			Neu
+		</Nav.item>
+		<Nav.item
+			href='/new'
+			color='green'
+			icon='fas fa-plus-square'>
+			Leer
+		</Nav.item>
+		<Nav.item
+			color='purple'
+			href='/new/EGy1wlD7TRl7'
+			icon='fas fa-plus-square'>
+			Abenteuer
+		</Nav.item>
+		<Nav.item
+		href='/new/GiL_1PHMlYZ5'
+		color='blue'
 		icon='fas fa-plus-square'>
-		Neu
-	</Nav.item>;
+			Spielhilfe
+		</Nav.item>
+		<Nav.item
+		href='/new/GiL_1PHMlYZ5'
+		color='orange'
+		icon='fas fa-plus-square'>
+			Kartendeck
+		</Nav.item>
+	</Nav.dropdown>;
 };
+
+
