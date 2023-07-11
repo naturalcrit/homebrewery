b30dda90b867c8c999f8d03a4b6908c74f8ad629
Lukas R <mail@sarbot.de>
2023-07-11 12:06:26 +0200

    hotfix remove blank class

--- themes/V3/Ilaris/snippets.js
+++ themes/V3/Ilaris/snippets.js
@@ -1,71 +1,71 @@
 /* eslint-disable max-lines */
 
-const MagicGen           = require('./snippets/magic.gen.js');
-const ClassTableGen      = require('./snippets/classtable.gen.js');
-const MonsterBlockGen    = require('./snippets/monsterblock.gen.js');
-const scriptGen          = require('./snippets/script.gen.js');
-const CoverPageGen       = require('./snippets/coverpage.gen.js');
+const MagicGen = require('./snippets/magic.gen.js');
+const ClassTableGen = require('./snippets/classtable.gen.js');
+const MonsterBlockGen = require('./snippets/monsterblock.gen.js');
+const scriptGen = require('./snippets/script.gen.js');
+const CoverPageGen = require('./snippets/coverpage.gen.js');
 const TableOfContentsGen = require('./snippets/tableOfContents.gen.js');
-const indexGen           = require('./snippets/index.gen.js');
-const dedent             = require('dedent-tabs').default;
+const indexGen = require('./snippets/index.gen.js');
+const dedent = require('dedent-tabs').default;
 
 
 
 module.exports = [
 
 	{
-		groupName : 'Text Editor',
-		icon      : 'fas fa-pencil-alt',
-		view      : 'text',
-		snippets  : [
+		groupName: 'Text Editor',
+		icon: 'fas fa-pencil-alt',
+		view: 'text',
+		snippets: [
 			{
-				name : 'Seitennummer',
-				icon : 'fas fa-bookmark',
-				gen  : '{{pageNumber 1}}\n{{footnote PART 1 | SECTION NAME}}\n\n'
+				name: 'Seitennummer',
+				icon: 'fas fa-bookmark',
+				gen: '{{pageNumber 1}}\n{{footnote PART 1 | SECTION NAME}}\n\n'
 			},
 			{
-				name : 'Selbstzählende Seitennummer',
-				icon : 'fas fa-sort-numeric-down',
-				gen  : '{{pageNumber,auto}}\n{{footnote PART 1 | SECTION NAME}}\n\n'
+				name: 'Selbstzählende Seitennummer',
+				icon: 'fas fa-sort-numeric-down',
+				gen: '{{pageNumber,auto}}\n{{footnote PART 1 | SECTION NAME}}\n\n'
 			},
 			{
-				name : 'Inhaltsverzeichnis',
-				icon : 'fas fa-book',
-				gen  : TableOfContentsGen
+				name: 'Inhaltsverzeichnis',
+				icon: 'fas fa-book',
+				gen: TableOfContentsGen
 			},
 			{
-				name         : 'Kopfzeile',
-				icon         : 'fas fa-minus',
-				gen          : function() {
+				name: 'Kopfzeile',
+				icon: 'fas fa-minus',
+				gen: function () {
 					return dedent`
 					{{kopfzeile Dies ist eine Kopfzeile}}`
 				},
 			},
 			{
-				name         : 'Fußzeile',
-				icon         : 'fas fa-minus',
-				gen          : function() {
+				name: 'Fußzeile',
+				icon: 'fas fa-minus',
+				gen: function () {
 					return dedent`
 					{{fusszeile Dies ist eine Fußzeile}}`
 				},
 			},
 			{
-				name         : 'Index',
-				icon         : 'fas fa-bars',
-				gen          : indexGen,
-				experimental : true
+				name: 'Index',
+				icon: 'fas fa-bars',
+				gen: indexGen,
+				experimental: true
 			}
 		]
 	},
 	{
-		groupName : 'Style Editor',
-		icon      : 'fas fa-pencil-alt',
-		view      : 'style',
-		snippets  : [
+		groupName: 'Style Editor',
+		icon: 'fas fa-pencil-alt',
+		view: 'style',
+		snippets: [
 			{
-				name : 'Remove Drop Cap',
-				icon : 'fas fa-remove-format',
-				gen  : dedent`/* Removes Drop Caps */
+				name: 'Remove Drop Cap',
+				icon: 'fas fa-remove-format',
+				gen: dedent`/* Removes Drop Caps */
 						.page h1+p:first-letter {
 							all: unset;
 						}\n\n
@@ -75,9 +75,9 @@ module.exports = [
 						}`
 			},
 			{
-				name : 'Tweak Drop Cap',
-				icon : 'fas fa-sliders-h',
-				gen  : dedent`/* Drop Cap settings */
+				name: 'Tweak Drop Cap',
+				icon: 'fas fa-sliders-h',
+				gen: dedent`/* Drop Cap settings */
 						.page h1 + p::first-letter {
 							font-family: SolberaImitationRemake;
 							font-size: 3.5cm;
@@ -90,14 +90,14 @@ module.exports = [
 
 	/*********************** IMAGES *******************/
 	{
-		groupName : 'Grafiken',
-		icon      : 'fas fa-images',
-		view      : 'text',
-		snippets  : [
+		groupName: 'Grafiken',
+		icon: 'fas fa-images',
+		view: 'text',
+		snippets: [
 			{
-				name : 'Bild',
-				icon : 'fas fa-image',
-				gen  : dedent`
+				name: 'Bild',
+				icon: 'fas fa-image',
+				gen: dedent`
 					![cat warrior](https://s-media-cache-ak0.pinimg.com/736x/4a/81/79/4a8179462cfdf39054a418efd4cb743e.jpg) {width:325px,mix-blend-mode:multiply}
 
 					{{artist,position:relative,top:-230px,left:10px,margin-bottom:-30px
@@ -106,9 +106,9 @@ module.exports = [
 					}}`
 			},
 			{
-				name : 'Hintergrundbild',
-				icon : 'fas fa-tree',
-				gen  : dedent`
+				name: 'Hintergrundbild',
+				icon: 'fas fa-tree',
+				gen: dedent`
 					![homebrew mug](http://i.imgur.com/hMna6G0.png) {position:absolute,top:50px,right:30px,width:280px}
 
 					{{artist,top:80px,right:30px
@@ -117,123 +117,123 @@ module.exports = [
 					}}`
 			},
 			{
-				name : 'Wasserzeichen',
-				icon : 'fas fa-id-card',
-				gen  : dedent`
+				name: 'Wasserzeichen',
+				icon: 'fas fa-id-card',
+				gen: dedent`
 				{{watermark Ilaris}}\n`
 			},
 			{
 				name: 'Icons',
 				icon: 'fas fa-icons',
-				gen: function() {return "{{symbol}}";},
-				subsnippets : [
+				gen: function () { return "{{symbol}}"; },
+				subsnippets: [
 					{
 						name: "Königin",
 						icon: 'fas fa-chess-queen',
-						gen: function() {return "{{symbol,koenigin}}";}
+						gen: function () { return "{{symbol,koenigin}}"; }
 					},
 					{
 						name: "Springer",
 						icon: 'fas fa-chess-knight',
-						gen: function() {return "{{symbol,springer}}";}
+						gen: function () { return "{{symbol,springer}}"; }
 					},
 					{
 						name: "Bauer",
 						icon: 'fas fa-chess-pawn',
-						gen: function() {return "{{symbol,bauer}}";}
+						gen: function () { return "{{symbol,bauer}}"; }
 					},
 					{
 						name: "Feder",
 						icon: 'fas fa-feather',
-						gen: function() {return "{{symbol,feder}}";}
+						gen: function () { return "{{symbol,feder}}"; }
 					},
 					{
 						name: "Medalie",
 						icon: 'fas fa-medal',
-						gen: function() {return "{{symbol,medalie}}";}
+						gen: function () { return "{{symbol,medalie}}"; }
 					},
 					{
 						name: "Helm",
 						icon: 'fas fa-hard-hat',
-						gen: function() {return "{{symbol,helm}}";}
+						gen: function () { return "{{symbol,helm}}"; }
 					},
 					{
 						name: "Schubkarre",
 						icon: 'fas fa-shopping-cart',
-						gen: function() {return "{{symbol,schubkarre}}";}
+						gen: function () { return "{{symbol,schubkarre}}"; }
 					},
 					{
 						name: "Kompass",
 						icon: 'far fa-compass',
-						gen: function() {return "{{symbol,kompass}}";}
+						gen: function () { return "{{symbol,kompass}}"; }
 					},
 					{
 						name: "Turm",
 						icon: 'fas fa-gopuram',
-						gen: function() {return "{{symbol,turm}}";}
+						gen: function () { return "{{symbol,turm}}"; }
 					},
 					{
 						name: "Schatz",
 						icon: 'far fa-gem',
-						gen: function() {return "{{symbol,schatz}}";}
+						gen: function () { return "{{symbol,schatz}}"; }
 					},
 				]
 			},
 			{
 				name: 'Logo',
 				icon: 'fas fa-dice-d20',
-				gen  : function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_rot.png)}}\n'; },
-				subsnippets  : [
+				gen: function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_rot.png)}}\n'; },
+				subsnippets: [
 					{
-						name : 'Würfel Rot',
-						icon : 'fas fa-dice-d20',
+						name: 'Würfel Rot',
+						icon: 'fas fa-dice-d20',
 						color: 'red',
-						gen  : function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_rot.png)}}\n'; }
+						gen: function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_rot.png)}}\n'; }
 					},
 					{
-						name : 'Würfel Braun',
-						icon : 'fas fa-dice-d20',
-						gen  : function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_braun.png)}}\n'; }
+						name: 'Würfel Braun',
+						icon: 'fas fa-dice-d20',
+						gen: function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_braun.png)}}\n'; }
 					},
 					{
-						name : 'Würfel Orange',
-						icon : 'fas fa-dice-d20',
-						gen  : function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_orange.png)}}\n'; }
+						name: 'Würfel Orange',
+						icon: 'fas fa-dice-d20',
+						gen: function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_orange.png)}}\n'; }
 					},
 					{
-						name : 'Würfel Gelb',
-						icon : 'fas fa-dice-d20',
-						gen  : function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_gelb.png)}}\n'; }
+						name: 'Würfel Gelb',
+						icon: 'fas fa-dice-d20',
+						gen: function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_gelb.png)}}\n'; }
 					},
 					{
-						name : 'Würfel Hellgrün',
-						icon : 'fas fa-dice-d20',
-						gen  : function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_hellgruen.png)}}\n'; }
+						name: 'Würfel Hellgrün',
+						icon: 'fas fa-dice-d20',
+						gen: function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_hellgruen.png)}}\n'; }
 					},
 					{
-						name : 'Würfel Grün',
-						icon : 'fas fa-dice-d20',
-						gen  : function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_gruen.png)}}\n'; }
+						name: 'Würfel Grün',
+						icon: 'fas fa-dice-d20',
+						gen: function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_gruen.png)}}\n'; }
 					},
 					{
-						name : 'Würfel Blau',
-						icon : 'fas fa-dice-d20',
-						gen  : function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_blau.png)}}\n'; }
+						name: 'Würfel Blau',
+						icon: 'fas fa-dice-d20',
+						gen: function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_blau.png)}}\n'; }
 					},
 					{
-						name : 'Würfel Hellblau',
-						icon : 'fas fa-dice-d20',
-						gen  : function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_hellblau.png)}}\n'; }
+						name: 'Würfel Hellblau',
+						icon: 'fas fa-dice-d20',
+						gen: function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_hellblau.png)}}\n'; }
 					},
 					{
-						name : 'Würfel Lila',
-						icon : 'fas fa-dice-d20',
-						gen  : function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_lila.png)}}\n'; }
+						name: 'Würfel Lila',
+						icon: 'fas fa-dice-d20',
+						gen: function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_lila.png)}}\n'; }
 					},
 					{
-						name : 'Würfel Pink',
-						icon : 'fas fa-dice-d20',
-						gen  : function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_pink.png)}}\n'; }
+						name: 'Würfel Pink',
+						icon: 'fas fa-dice-d20',
+						gen: function () { return '{{logo ![Ilaris Würfel-Logo](/assets/ilaris/icon_pink.png)}}\n'; }
 					},
 				]
 			}
@@ -242,26 +242,26 @@ module.exports = [
 
 	/************************* ILARIS ********************/
 	{
-		groupName : 'Ilaris',
-		icon      : 'fas fa-book',
-		view      : 'text',
-		snippets  : [
+		groupName: 'Ilaris',
+		icon: 'fas fa-book',
+		view: 'text',
+		snippets: [
 			{
-				name : 'Versalie',
-				icon : 'fas fa-archway',
-				gen  : function() {
+				name: 'Versalie',
+				icon: 'fas fa-archway',
+				gen: function () {
 					return '{{versalie A}}';
 				},
 			},
 			{
-				name : 'Zauber',
-				icon : 'fas fa-magic',
-				gen  : MagicGen.spell,
+				name: 'Zauber',
+				icon: 'fas fa-magic',
+				gen: MagicGen.spell,
 			},
 			{
-				name : 'Notiz',
-				icon : 'fas fa-sticky-note',
-				gen  : function(){
+				name: 'Notiz',
+				icon: 'fas fa-sticky-note',
+				gen: function () {
 					return dedent`
 						{{note
 						##### Time to Drop Knowledge
@@ -273,9 +273,9 @@ module.exports = [
 				},
 			},
 			{
-				name : 'Textbox',
-				icon : 'fas fa-comment-alt',
-				gen  : function(){
+				name: 'Textbox',
+				icon: 'fas fa-comment-alt',
+				gen: function () {
 					return dedent`
 						{{textbox
 						##### Hier kann was wichtiges stehen
@@ -291,9 +291,9 @@ module.exports = [
 				},
 			},
 			{
-				name : 'Kasten - Brief',
-				icon : 'fas fa-envelope',
-				gen  : function(){
+				name: 'Kasten - Brief',
+				icon: 'fas fa-envelope',
+				gen: function () {
 					return dedent`
 					{{kasten,handschrift
 					Edle Helden,
@@ -306,20 +306,20 @@ module.exports = [
 				},
 			},
 			{
-				name : 'Kreatur',
-				icon : 'fas fa-paw',
-				gen  : MonsterBlockGen.creature(),
-				experimental : true
+				name: 'Kreatur',
+				icon: 'fas fa-paw',
+				gen: MonsterBlockGen.creature(),
+				experimental: true
 			},
 			{
-				name : 'NSC',
-				icon : 'fas fa-user-secret',  // fa-masks-theater
-				gen  : MonsterBlockGen.humanoid(),
+				name: 'NSC',
+				icon: 'fas fa-user-secret',  // fa-masks-theater
+				gen: MonsterBlockGen.humanoid(),
 			},
 			{
-				name : 'Künstler Credit',
-				icon : 'fas fa-signature',
-				gen  : function(){
+				name: 'Künstler Credit',
+				icon: 'fas fa-signature',
+				gen: function () {
 					return dedent`
 						{{artist,top:90px,right:30px
 						##### Starry Night
@@ -331,7 +331,7 @@ module.exports = [
 			{
 				name: 'Ulisses Disclaimer',
 				icon: 'far fa-copyright',
-				gen : function () {
+				gen: function () {
 					return dedent`
 					{{credit
 					#### Artwork © ${new Date().getFullYear()} Ulisses Spiele.  
@@ -349,60 +349,60 @@ module.exports = [
 
 	/*********************  TABLES *********************/
 	{
-		groupName : 'Tabellen',
-		icon      : 'fas fa-table',
-		view      : 'text',
-		snippets  : [
+		groupName: 'Tabellen',
+		icon: 'fas fa-table',
+		view: 'text',
+		snippets: [
 			{
-				name : 'Klassentabelle',
-				icon : 'fas fa-table',
-				gen  : ClassTableGen.full('classTable,frame,decoration,wide'),
+				name: 'Klassentabelle',
+				icon: 'fas fa-table',
+				gen: ClassTableGen.full('classTable,frame,decoration,wide'),
 			},
 			{
-				name : 'Klassentabelle (randlos)',
-				icon : 'fas fa-border-none',
-				gen  : ClassTableGen.full('classTable,wide'),
+				name: 'Klassentabelle (randlos)',
+				icon: 'fas fa-border-none',
+				gen: ClassTableGen.full('classTable,wide'),
 			},
 			{
-				name : '1/2 Klassentabelle',
-				icon : 'fas fa-list-alt',
-				gen  : ClassTableGen.half('classTable,decoration,frame'),
+				name: '1/2 Klassentabelle',
+				icon: 'fas fa-list-alt',
+				gen: ClassTableGen.half('classTable,decoration,frame'),
 			},
 			{
-				name : '1/2 Klassentabelle (randlos)',
-				icon : 'fas fa-border-none',
-				gen  : ClassTableGen.half('classTable'),
+				name: '1/2 Klassentabelle (randlos)',
+				icon: 'fas fa-border-none',
+				gen: ClassTableGen.half('classTable'),
 			},
 			{
-				name : '1/3 Klassentabelle',
-				icon : 'fas fa-border-all',
-				gen  : ClassTableGen.third('classTable,frame'),
+				name: '1/3 Klassentabelle',
+				icon: 'fas fa-border-all',
+				gen: ClassTableGen.third('classTable,frame'),
 			},
 			{
-				name : '1/3 Klassentabelle (randlos)',
-				icon : 'fas fa-border-none',
-				gen  : ClassTableGen.third('classTable'),
+				name: '1/3 Klassentabelle (randlos)',
+				icon: 'fas fa-border-none',
+				gen: ClassTableGen.third('classTable'),
 			},
 			{
-				name         : 'Runen',
-				icon         : 'fas fa-language',
-				gen          : scriptGen.dwarvish,
-				experimental : true,
-				subsnippets  : [
+				name: 'Runen',
+				icon: 'fas fa-language',
+				gen: scriptGen.dwarvish,
+				experimental: true,
+				subsnippets: [
 					{
-						name : 'Zwergisch',
-						icon : 'fac davek',
-						gen  : scriptGen.dwarvish,
+						name: 'Zwergisch',
+						icon: 'fac davek',
+						gen: scriptGen.dwarvish,
 					},
 					{
-						name : 'Elfisch',
-						icon : 'fac rellanic',
-						gen  : scriptGen.elvish,
+						name: 'Elfisch',
+						icon: 'fac rellanic',
+						gen: scriptGen.elvish,
 					},
 					{
-						name : 'Drachisch',
-						icon : 'fac iokharic',
-						gen  : scriptGen.draconic,
+						name: 'Drachisch',
+						icon: 'fac iokharic',
+						gen: scriptGen.draconic,
 					},
 				]
 			},
@@ -411,29 +411,29 @@ module.exports = [
 
 	/*********************  Seiten *********************/
 	{
-		groupName : 'Seiten',
-		icon      : 'fas fa-table',
-		view      : 'text',
-		snippets  : [
+		groupName: 'Seiten',
+		icon: 'fas fa-table',
+		view: 'text',
+		snippets: [
 			{
-				name         : 'Vorderseite - Vollbild',
-				icon         : 'fac book-front-cover',
-				gen          : CoverPageGen.front,
+				name: 'Vorderseite - Vollbild',
+				icon: 'fac book-front-cover',
+				gen: CoverPageGen.front,
 			},
 			{
-				name         : 'Vorderseite - Teilbild',
-				icon         : 'fac book-part-cover',
-				gen          : CoverPageGen.frontPart,
+				name: 'Vorderseite - Teilbild',
+				icon: 'fac book-part-cover',
+				gen: CoverPageGen.frontPart,
 			},
 			{
-				name      		 : 'Rückseite - Teilbild',
-				icon      		 : 'fac book-back-cover',
-				gen       		 : CoverPageGen.backRight,
+				name: 'Rückseite - Teilbild',
+				icon: 'fac book-back-cover',
+				gen: CoverPageGen.backRight,
 			},
 			{
-				name      		 : 'Rückseite - Bild oben',
-				icon      		 : 'fac book-back-cover',
-				gen       		 : CoverPageGen.backTop,
+				name: 'Rückseite - Bild oben',
+				icon: 'fac book-back-cover',
+				gen: CoverPageGen.backTop,
 			},
 			{
 				name: "Dank und Copyright",
@@ -449,7 +449,7 @@ module.exports = [
 			{
 				name: 'Einzelkarte',
 				icon: 'far fa-square',
-				gen : function(){
+				gen: function () {
 					return dedent`
 						\page
 						{{einzelkarte}}
@@ -463,14 +463,14 @@ module.exports = [
 				},
 			},
 			{
-				name      		 : 'Karten (3x3)',
-				icon      		 : 'fas fa-th',
-				gen       		 : function() {
+				name: 'Karten (3x3)',
+				icon: 'fas fa-th',
+				gen: function () {
 					return dedent`
 						\page
 						{{kopfzeile Karten}}
 						{{karten}}
-						{{blank}}
+						{{keinhintergrund}}
 			
 						{{karte,schwarz
 						#### Randfarben
@@ -572,14 +572,14 @@ module.exports = [
 	/**************** PAGE *************/
 
 	{
-		groupName : 'Druck',
-		icon      : 'fas fa-print',
-		view      : 'style',
-		snippets  : [
+		groupName: 'Druck',
+		icon: 'fas fa-print',
+		view: 'style',
+		snippets: [
 			{
-				name : 'Toner-schonend',
-				icon : 'fas fa-tint',
-				gen  : dedent`
+				name: 'Toner-schonend',
+				icon: 'fas fa-tint',
+				gen: dedent`
 					/* Ink Friendly */
 					*:is(.page,.monster,.note,.descriptive) {
 						background : white !important;
