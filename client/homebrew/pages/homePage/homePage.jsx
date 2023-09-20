3c3b728b59c60928e4d8706e1f5c1b815aec3e54
Gazook89 <John.W.Jones89@gmail.com>
2023-09-20 13:58:32 -0500

    Get basic homepage workign with new navbar.
    remove extra line.

--- client/homebrew/pages/homePage/homePage.jsx
+++ client/homebrew/pages/homePage/homePage.jsx
@@ -6,13 +6,20 @@ const cx = require('classnames');
 const request = require('../../utils/request-middleware.js');
 const { Meta } = require('vitreum/headtags');
 
-const Nav = require('naturalcrit/nav/nav.jsx');
 const Navbar = require('../../navbar/navbar.jsx');
-const NewBrewItem = require('../../navbar/newBrew.jsx');
-const HelpNavItem = require('../../navbar/help.navitem.jsx');
-const RecentNavItem = require('../../navbar/recent.navitem.jsx').both;
-const AccountNavItem = require('../../navbar/account.navitem.jsx');
+import * as Toolbar from '@radix-ui/react-toolbar';
+import * as Menubar from '@radix-ui/react-menubar';
+import { LinkItem, ButtonItem, Menu, SubMenu } from '../../navbar/menubarExtensions.jsx';
+import { getRedditLink } from '../../navbar/getRedditLink.jsx';
+import { RecentItems } from '../../navbar/recent.navitem.jsx';
+
+const BrewTitle = require('../../navbar/brewTitle.jsx');
+const NewBrew = require('../../navbar/newBrew.jsx');
+const HelpItems = require('../../navbar/help.navitem.jsx');
+const PrintLink = require('../../navbar/print.navitem.jsx');
 const ErrorNavItem = require('../../navbar/error-navitem.jsx');
+const Account = require('../../navbar/account.navitem.jsx');
+const Tabs = require('../../editor/editorTabs.jsx')
 
 
 const SplitPane = require('naturalcrit/splitPane/splitPane.jsx');
@@ -48,35 +55,138 @@ const HomePage = createClass({
 				window.location = `/edit/${brew.editId}`;
 			});
 	},
-	handleSplitMove : function(){
-		this.refs.editor.update();
-	},
+	// handleSplitMove : function(){
+	// 	this.refs.editor.update();
+	// },
 	handleTextChange : function(text){
 		this.setState((prevState)=>({
 			brew : { ...prevState.brew, text: text }
 		}));
 	},
-	renderNavbar : function(){
-		return <Navbar ver={this.props.ver}>
-			<Nav.section>
-				{this.state.error ?
-					<ErrorNavItem error={this.state.error} parent={this}></ErrorNavItem> :
-					null
-				}
-				<NewBrewItem />
-				<HelpNavItem />
-				<RecentNavItem />
-				<AccountNavItem />
-			</Nav.section>
-		</Navbar>;
+	renderNavbar : function(menubar) {
+		// If the screen is 'full size'...
+		if(this.props.isNarrow == false){
+
+			if(menubar.location === 'bottom') return;
+
+			return <Navbar.Top>
+				<Menu trigger='Brew'>
+					<NewBrew />
+					{/* <SubMenu trigger='Save'>
+						{this.renderSaveButton()}
+						{this.renderAutoSaveButton()}
+					</SubMenu> */}
+					<Menubar.Separator />
+					<SubMenu trigger='Recently Edited'>
+						<RecentItems brew={this.state.brew} storageKey='edit' />
+					</SubMenu>
+					<SubMenu trigger='Recently Viewed'>
+						<RecentItems brew={this.state.brew} storageKey='view' />
+					</SubMenu>
+					{global.account ? <LinkItem href={`/user/${encodeURI(global.account.username)}`}>Library</LinkItem> : ''}
+					{/* <Menubar.Separator /> */}
+					{/* <SubMenu trigger='Share'>
+						<LinkItem href={`/share/${this.processShareId()}`}
+							target='_blank'
+							rel='noopener noreferrer'>Go To Share Page</LinkItem>
+						<LinkItem href={getRedditLink(this.state.brew)}
+							target='_blank'
+							rel='noopener noreferrer'>Share On Reddit</LinkItem>
+						<ButtonItem onClick={()=>{navigator.clipboard.writeText(`${global.config.publicUrl}/share/${this.processShareId()}`);}}>Copy Share URL</ButtonItem>
+					</SubMenu> */}
+					{/* <PrintLink shareId={this.processShareId()}>Print</PrintLink> */}
+				</Menu>
+				<Menu trigger='Help'>
+					<HelpItems.faq />
+					<HelpItems.migrate />
+					<Menubar.Separator />
+					<SubMenu trigger='Community'>
+						<HelpItems.rHomebrewery />
+						<HelpItems.DoMT />
+					</SubMenu>
+					<Menubar.Separator />
+					<SubMenu trigger='Report Issue'>
+						<HelpItems.issueToReddit />
+						<HelpItems.issueToGithub />
+					</SubMenu>
+				</Menu>
+				<BrewTitle title={this.state.brew.title} />
+				<Account />
+			</Navbar.Top>;
+
+		} else {    // If the screen is narrow (such as on mobile)...
+
+			if(menubar.location === 'top'){
+				return <Navbar.Top>
+					<BrewTitle title={this.state.brew.title} />
+					<Menu trigger='Help'>
+						<HelpItems.faq />
+						<HelpItems.migrate />
+						<Menubar.Separator />
+						<SubMenu trigger='Community'>
+							<HelpItems.rHomebrewery />
+							<HelpItems.DoMT />
+						</SubMenu>
+						<Menubar.Separator />
+						<SubMenu trigger='Report Issue'>
+							<HelpItems.issueToReddit />
+							<HelpItems.issueToGithub />
+						</SubMenu>
+					</Menu>
+				</Navbar.Top>;
+			} else {
+				return <Navbar.Bottom>
+					<Menu trigger='Brew'>
+						{/* <NewBrew />
+						<SubMenu trigger='Save'>
+							{this.renderSaveButton()}
+							{this.renderAutoSaveButton()}
+						</SubMenu> */}
+						{/* <Menubar.Separator /> */}
+						<SubMenu trigger='Recently Edited'>
+							<RecentItems brew={this.state.brew} storageKey='edit' />
+						</SubMenu>
+						<SubMenu trigger='Recently Viewed'>
+							<RecentItems brew={this.state.brew} storageKey='view' />
+						</SubMenu>
+						{global.account ? <LinkItem href={`/user/${encodeURI(global.account.username)}`}>Library</LinkItem> : ''}
+						{/* <Menubar.Separator /> */}
+						{/* <PrintLink shareId={this.processShareId()}>Print</PrintLink> */}
+					</Menu>
+					<Account />
+				</Navbar.Bottom>;
+			}
+		}
+
 	},
 
 	render : function(){
 		return <div className='homePage sitePage'>
 			<Meta name='google-site-verification' content='NwnAQSSJZzAT7N-p5MY6ydQ7Njm67dtbu73ZSyE5Fy4' />
-			{this.renderNavbar()}
+			{this.renderNavbar({ location: 'top' })}
 
 			<div className='content'>
+				<SplitPane onDragFinish={this.handleSplitMove} ref='pane'>
+					<Tabs
+						brew={this.state.brew}
+						onTextChange={this.handleTextChange}
+						onStyleChange={this.handleStyleChange}
+						onMetaChange={this.handleMetaChange}
+						reportError={this.errorReported}
+						renderer={this.state.brew.renderer} 
+					/>
+					<BrewRenderer
+						text={this.state.brew.text}
+						style={this.state.brew.style}
+						renderer={this.state.brew.renderer}
+						theme={this.state.brew.theme}
+						errors={this.state.htmlErrors}
+						lang={this.state.brew.lang}
+					/>
+				</SplitPane>
+			</div>
+
+			{/* <div className='content'>
 				<SplitPane onDragFinish={this.handleSplitMove} ref='pane'>
 					<Editor
 						ref='editor'
@@ -87,7 +197,7 @@ const HomePage = createClass({
 					/>
 					<BrewRenderer text={this.state.brew.text} style={this.state.brew.style} renderer={this.state.brew.renderer}/>
 				</SplitPane>
-			</div>
+			</div> */}
 
 			<div className={cx('floatingSaveButton', { show: this.state.welcomeText != this.state.brew.text })} onClick={this.handleSave}>
 				Save current <i className='fas fa-save' />
@@ -96,6 +206,8 @@ const HomePage = createClass({
 			<a href='/new' className='floatingNewButton'>
 				Create your own <i className='fas fa-magic' />
 			</a>
+
+			{this.renderNavbar({ location: 'bottom' })}
 		</div>;
 	}
 });
