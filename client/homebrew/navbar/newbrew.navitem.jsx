83ba1fbb6abb24f3adfb2310b72b500f7368e479
G.Ambatte <sean@robertson-family.nz>
2023-12-21 14:12:31 +1300

    New menu item

--- client/homebrew/navbar/newbrew.navitem.jsx
+++ client/homebrew/navbar/newbrew.navitem.jsx
@@ -1,11 +1,13 @@
 const React = require('react');
 const Nav = require('naturalcrit/nav/nav.jsx');
 
+const translateOpts = ['nav', 'newDoc'];
+
 module.exports = function(props){
 	return <Nav.item
 		href='/new'
 		color='purple'
 		icon='fas fa-plus-square'>
-		new
+		{'new'.translate(translateOpts)}
 	</Nav.item>;
 };
