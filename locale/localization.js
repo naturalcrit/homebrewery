7f01546ee4cb107d9b6d7c0cfeca9f7995469141
G.Ambatte <sean@robertson-family.nz>
2023-12-28 10:50:39 +1300

    Add default functions

--- locale/localization.js
+++ locale/localization.js
@@ -2,7 +2,7 @@
 module.exports = {
 	addTranslationFunc : function (localeData){
 		// Add translation function to String prototype
-		String.prototype.translate = function(_groups) {
+		String.prototype.translate = function(_groups = String.prototype.getTranslationDefaults()) {
 			const text = this.valueOf();
 			const groups = _groups ? Array.from(_groups) : [];
 			let obj = localeData;
@@ -17,6 +17,14 @@ module.exports = {
 
 			if(obj[text] && obj[text].length > 0) return obj[text];
 			return text;
+		};
+
+		// Add defaults
+		String.prototype.getTranslationDefaults = function() {
+			return String.prototype.translationDefaults || [];
+		};
+		String.prototype.setTranslationDefaults = function(_groups=[]) {
+			String.prototype.translationDefaults = _groups;
 		};
 	}
 };
