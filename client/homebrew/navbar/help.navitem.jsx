6efb23c0a70bed0959a65eb61101feb104c86a9d
G.Ambatte <sean@robertson-family.nz>
2024-01-08 22:01:21 +1300

    Update Help Nav items

--- client/homebrew/navbar/help.navitem.jsx
+++ client/homebrew/navbar/help.navitem.jsx
@@ -5,10 +5,13 @@ const dedent = require('dedent-tabs').default;
 
 const Nav = require('naturalcrit/nav/nav.jsx');
 
+const translateOpts = ['nav', 'helpDropdown'];
+
 module.exports = function(props){
+	''.setTranslationDefaults(translateOpts);
 	return <Nav.dropdown>
 		<Nav.item color='grey' icon='fas fa-question-circle'>
-			need help?
+			{'needHelp'.translate()}
 		</Nav.item>
 		<Nav.item color='red' icon='fas fa-fw fa-bug'
 			href={`https://www.reddit.com/r/homebrewery/submit?selftext=true&text=${encodeURIComponent(dedent`
@@ -18,19 +21,19 @@ module.exports = function(props){
 			- **Issue** :  `)}`}
 			newTab={true}
 			rel='noopener noreferrer'>
-			report issue
+			{'report'.translate()}
 		</Nav.item>
 		<Nav.item color='green' icon='fas fa-question-circle'
 			href='/faq'
 			newTab={true}
 			rel='noopener noreferrer'>
-			FAQ
+			{'faq'.translate()}
 		</Nav.item>
 		<Nav.item color='blue' icon='fas fa-fw fa-file-import'
 			href='/migrate'
 			newTab={true}
 			rel='noopener noreferrer'>
-			migrate
+			{'migrate'.translate()}
 		</Nav.item>
 	</Nav.dropdown>;
 };
