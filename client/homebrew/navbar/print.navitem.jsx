20d89c9ba2089050d8206970847e58e3014d1730
G.Ambatte <sean@robertson-family.nz>
2024-01-08 18:27:11 +1300

    Update Print Nav item

--- client/homebrew/navbar/print.navitem.jsx
+++ client/homebrew/navbar/print.navitem.jsx
@@ -2,8 +2,11 @@ const React = require('react');
 const createClass = require('create-react-class');
 const Nav = require('naturalcrit/nav/nav.jsx');
 
+const translateOpts = ['nav'];
+
 module.exports = function(props){
+	''.setTranslationDefaults(translateOpts);
 	return <Nav.item newTab={true} href={`/print/${props.shareId}?dialog=true`} color='purple' icon='far fa-file-pdf'>
-		get PDF
+		{'getPdf'.translate()}
 	</Nav.item>;
 };
