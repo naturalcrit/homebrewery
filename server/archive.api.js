9aa5eea8c9f98141654c56d49c2401cceb57b04a
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2024-01-27 23:31:56 +0100

    exclude fields and add a time limit

--- server/archive.api.js
+++ server/archive.api.js
@@ -10,40 +10,28 @@ const archive = {
             const limit = 2000;
             const brews = await HomebrewModel.find({
                 title: { $regex: req.params.query, $options: 'i' },
+                editId:0,
+                googleId:0,
+                text:0,
+                textBin:0,
                 published: true
             })
             .limit(limit)
+            .maxTimeMS(10000)
             .exec();
 
-            const simplifiedBrews = brews.map(brew => ({
-                title           : brew.title,
-                description     : brew.description,
-                authors         : brew.authors,
-                tags            : brew.tags,
-                pageCount       : brew.pageCount,
-                systems         : brew.systems,
-                renderer        : brew.renderer,
-                thumbnail       : brew.thumbnail,
-                createdAt       : brew.createdAt,
-                updatedAt       : brew.updatedAt,
-                lastViewed      : brew.lastViewed,
-                views           : brew.views,
-                shareId         : brew.shareId,
-                googleId        : brew.googleId
-            }))
-
-            if (!simplifiedBrews || simplifiedBrews.length === 0) {
+            if (!brews || brews.length === 0) {
                 // No published documents found with the given title
                 return res.status(404).json({ error: 'Published documents not found' });
             }
 
             let message = '';
-            if (simplifiedBrews.length === limit) {
+            if (brews.length === limit) {
                 // If the limit has been reached, include a message in the response
                 message = `You've reached the limit of ${limit} documents, you can try being more specific in your search.`;
             }
 
-            return res.json({ simplifiedBrews, message });
+            return res.json({ brews, message });
         } catch (error) {
             console.error(error);
             return res.status(500).json({ error: 'Internal Server Error' });
