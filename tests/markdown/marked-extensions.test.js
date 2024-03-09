e36a638ae588dcf04eee6676023e2984949f788d
Trevor Buckner <calculuschild@gmail.com>
2024-03-09 16:41:05 -0500

    Add more tests

--- tests/markdown/marked-extensions.test.js
+++ tests/markdown/marked-extensions.test.js
@@ -2,52 +2,60 @@
 
 const Markdown = require('naturalcrit/markdown.js');
 
-describe('Definition Terms', ()=>{
-	test('Single Definition', function() {
+describe('Inline Definition Lists', ()=>{
+	test('Single Definition Term', function() {
 		const source = 'My term :: My First Definition\n\n';
 		const rendered = Markdown.render(source);
 		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe('<dl><dt>My term</dt><dd>My First Definition</dd></dl>');
 	});
 
-	test('Multiple Definition Terms, single line, single definition', function() {
+	test('Multiple Definition Terms', function() {
 		const source = 'Term 1::Definition of Term 1\nTerm 2::Definition of Term 2\n\n';
 		const rendered = Markdown.render(source);
 		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe('<dl><dt>Term 1</dt><dd>Definition of Term 1</dd>\n<dt>Term 2</dt><dd>Definition of Term 2</dd></dl>');
 	});
+});
 
-	test('PANdoc style list - Single Term, Single Definition', function() {
+describe('Multiline Definition Lists', ()=>{
+	test('Single Term, Single Definition', function() {
 		const source = 'Term 1\n::Definition 1\n\n';
 		const rendered = Markdown.render(source);
 		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe('<dl><dt>Term 1</dt>\n<dd>Definition 1</dd></dl>');
 	});
 
-	test('PANdoc style list - Single Term, Plural Definitions', function() {
+	test('Single Term, Plural Definitions', function() {
 		const source = 'Term 1\n::Definition 1\n::Definition 2\n\n';
 		const rendered = Markdown.render(source);
 		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe('<dl><dt>Term 1</dt>\n<dd>Definition 1</dd>\n<dd>Definition 2</dd></dl>');
 	});
 
-	test('PANdoc style list - Multiple Term, Single Definitions', function() {
-		const source = 'Term 1\n::Definition 1\nTerm 2\n::Definition 1\n\n';
+	test('Multiple Term, Single Definitions', function() {
+		const source = 'Term 1\n::Definition 1\n\nTerm 2\n::Definition 1\n\n';
 		const rendered = Markdown.render(source);
 		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe('<dl><dt>Term 1</dt>\n<dd>Definition 1</dd>\n<dt>Term 2</dt>\n<dd>Definition 1</dd></dl>');
 	});
 
-	test('PANdoc style list - Multiple Term, Plural Definitions', function() {
-		const source = 'Term 1\n::Definition 1\n::Definition 2\nTerm 2\n::Definition 1\n::Definition 2\n\n';
+	test('Multiple Term, Plural Definitions', function() {
+		const source = 'Term 1\n::Definition 1\n::Definition 2\n\nTerm 2\n::Definition 1\n::Definition 2\n\n';
 		const rendered = Markdown.render(source);
 		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe('<dl><dt>Term 1</dt>\n<dd>Definition 1</dd>\n<dd>Definition 2</dd>\n<dt>Term 2</dt>\n<dd>Definition 1</dd>\n<dd>Definition 2</dd></dl>');
 	});
 
-	test('PANdoc style list - Single Term, Single multiple line definition', function() {
+	test('Single Term, Single multi-line definition', function() {
 		const source = 'Term 1\n::Definition 1\nand more and\nmore and more\n\n';
 		const rendered = Markdown.render(source);
 		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe('<dl><dt>Term 1</dt>\n<dd>Definition 1 and more and more and more</dd></dl>');
 	});
 
-	test('PANdoc style list - Multiple Term, Single multiple line definition', function() {
-		const source = 'Term 1\n::Definition 1\nand more and\nmore and more\n\n::Definition 2\n\n::Definition 3\n\n';
+	test('Single Term, Plural multi-line definitions', function() {
+		const source = 'Term 1\n::Definition 1\nand more and more\n::Definition 2\n\n::Definition 3\n\n';
 		const rendered = Markdown.render(source);
-		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe('<dl><dt>Term 1</dt>\n<dd>Definition 1 and more and more and more</dd>\n<dd>Definition 2</dd>\n<dd>Definition 3</dd></dl>');
+		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe('<dl><dt>Term 1</dt>\n<dd>Definition 1 and more and more</dd>\n<dd>Definition 2</dd>\n<dd>Definition 3</dd></dl>');
+	});
+
+	test('Multiple Term, Single multi-line definition', function() {
+		const source = 'Term 1\n::Definition 1\nand more and more\n\nTerm 2\n::Definition 1\n\n::Definition 2\n\n';
+		const rendered = Markdown.render(source);
+		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe('<dl><dt>Term 1</dt>\n<dd>Definition 1 and more and more</dd>\n<dt>Term 2</dt>\n<dd>Definition 1</dd>\n<dd>Definition 2</dd></dl>');
 	});
 });
