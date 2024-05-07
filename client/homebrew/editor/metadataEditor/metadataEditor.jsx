872ee339da31d0848363db1790feb291bc157541
David Bolack <dbolack@gmail.com>
2024-05-07 12:13:57 -0500

    Clean up console logs
    Eliminate erroronous theme pulldown texture load.

--- client/homebrew/editor/metadataEditor/metadataEditor.jsx
+++ client/homebrew/editor/metadataEditor/metadataEditor.jsx
@@ -202,7 +202,7 @@ const MetadataEditor = createClass({
 				const preview = theme?.thumbnail ? theme.thumbnail : `/themes/${theme.renderer}/${theme.path}/dropdownPreview.png`;
 				return <div className='item' key={`${renderer}_${theme.name}`} onClick={()=>this.handleTheme(theme, renderer)} title={''}>
 					{`${renderer} : ${theme.name}`}
-					<img src={`/themes/${theme.renderer}/${theme.path}/dropdownTexture.png`}/>
+					<img src={`/themes/${theme.renderer}/${theme.path[0] == '#' ? '5ePHB' : theme.path}/dropdownTexture.png`}/>
 					<div className='preview'>
 						<h6>{`${theme.name}`} preview</h6>
 						<img src={`${preview}`}/>
