748c25aae40a4c6874b97d45a6a5463519a77caf
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2024-05-24 20:42:25 +0200

    "Added express-rate-limit package and implemented rate limiting for admin API login attempts"

--- server/admin.api.js
+++ server/admin.api.js
@@ -1,29 +1,39 @@
 const HomebrewModel = require('./homebrew.model.js').model;
 const router = require('express').Router();
 const Moment = require('moment');
-//const render = require('vitreum/steps/render');
 const templateFn = require('../client/template.js');
 const zlib = require('zlib');
+const rateLimit = require('express-rate-limit');
+
+// Define rate limiter options
+const loginLimiter = rateLimit({
+    windowMs: 24 * 60 * 60 * 1000, // 24 hours window
+    max: 10, // limit each IP to 10 requests per windowMs
+    message: "Too many login attempts from this IP, please try again later"
+});
 
 process.env.ADMIN_USER = process.env.ADMIN_USER || 'admin';
 process.env.ADMIN_PASS = process.env.ADMIN_PASS || 'password3';
 
 const mw = {
-	adminOnly : (req, res, next)=>{
-		if(!req.get('authorization')){
-			return res
-				.set('WWW-Authenticate', 'Basic realm="Authorization Required"')
-				.status(401)
-				.send('Authorization Required');
-		}
-		const [username, password] = Buffer.from(req.get('authorization').split(' ').pop(), 'base64')
-			.toString('ascii')
-			.split(':');
-		if(process.env.ADMIN_USER === username && process.env.ADMIN_PASS === password){
-			return next();
-		}
-		return res.status(401).send('Access denied');
-	}
+    adminOnly: [
+        loginLimiter,
+        (req, res, next) => {
+            if (!req.get('authorization')) {
+                return res
+                    .set('WWW-Authenticate', 'Basic realm="Authorization Required"')
+                    .status(401)
+                    .send('Authorization Required');
+            }
+            const [username, password] = Buffer.from(req.get('authorization').split(' ').pop(), 'base64')
+                .toString('ascii')
+                .split(':');
+            if (process.env.ADMIN_USER === username && process.env.ADMIN_PASS === password) {
+                return next();
+            }
+            return res.status(401).send('Access denied');
+        }
+    ]
 };
 
 const junkBrewPipeline = [
