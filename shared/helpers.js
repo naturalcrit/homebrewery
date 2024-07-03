5db86fd368577a79406b7619eb0881e2d5177e84
David Bolack <dbolack@gmail.com>
2024-07-03 15:57:30 -0500

    Exporting updates.
    This changes the PDF print option to an Export menu with PDF as the first choice and CBZ as the second.
    
    CBZ Export is functional, but not quite perfect. There are a few border effects that do not seem to work correctly with the image export.
    
    As the export progresses the left margin will vanish while it processes to account for the the canvas convertor interpretting the margin. As a plus this functions as a progress meter. :)

--- shared/helpers.js
+++ shared/helpers.js
@@ -1,5 +1,39 @@
 const _ = require('lodash');
 const yaml = require('js-yaml');
+const jszip = require('jszip');
+const fileSaver = require('file-saver');
+const htmlimg = require('html-to-image');
+const base64url = require('base64-url');
+
+
+const thumbnailCapture = async (pageNumber, brewRenderer)=>{
+
+	const activePage = brewRenderer.getElementById(`p${pageNumber.toString()}`);
+	const srcImages = activePage.getElementsByTagName('img');
+	for (let imgPos = 0; imgPos < srcImages.length; imgPos++) {
+		srcImages[imgPos].src = !srcImages[imgPos].src.startsWith(window.location.host) ? `/xssp/${base64url.encode(srcImages[imgPos].src)}` : srcImages[imgPos].src;
+	}
+
+	const clientHeightLg = activePage.clientHeight;
+	const clientWidthLg = activePage.clientWidth;
+
+	// HARD Override margins.
+	const lastMargin = activePage.style.marginLeft;
+	activePage.style.marginLeft = '0px';
+
+	return await htmlimg.toJpeg(activePage,
+		{
+		  width        : clientWidthLg,
+		  height       : clientHeightLg,
+		  canvasHeight : clientHeightLg,
+		  canvasWidth  : clientWidthLg,
+		  quality      : 0.75
+		}).then(async (dataURL)=>{
+		const blob = await (await fetch(dataURL)).blob();
+		activePage.style.marginLeft = lastMargin;
+		return blob;
+	});
+};
 
 const splitTextStyleAndMetadata = (brew)=>{
 	brew.text = brew.text.replaceAll('\r\n', '\n');
@@ -28,7 +62,53 @@ const printCurrentBrew = ()=>{
 	}
 };
 
+const createBrewCBZ = async ()=>{
+	const thisHost = `${window.location.protocol}//${window.location.host}`;
+
+	function urlReplacer(urlMatch, url) {
+		if(!url.startsWith(thisHost)) return (`url(/xssp/${base64url.encode(url)})`);
+		else return url;
+	}
+
+	if(window.typeof !== 'undefined') {
+		const bR = parent.document.getElementById('BrewRenderer');
+		const brewRenderer = bR.contentDocument || bR.contentWindow.document;
+		const numberOfPages = brewRenderer.getElementsByClassName('page').length + 1;
+		const topLinks = brewRenderer.getElementsByTagName('link');
+		const topStyles = brewRenderer.getElementsByTagName('style');
+		for (let linkPos = 0; linkPos < topLinks.length; linkPos++) {
+			if(!topLinks[linkPos].href.startsWith(thisHost)) {
+				topLinks[linkPos].href = `${thisHost}/xssp/${base64url.encode(topLinks[linkPos].href)}`;
+			}
+		}
+		for (let stylePos = 0; stylePos < topStyles.length; stylePos++) {
+			const urlRegex = /url\(([^\'\"].*[^\'\"])\)/gs;
+			const urlRegexWrapped = /url\(\'(.*)\'\)/gs;
+			topStyles[stylePos].innerText = topStyles[stylePos].innerText.replace(urlRegex,  urlReplacer);
+			topStyles[stylePos].innerText = topStyles[stylePos].innerText.replace(urlRegexWrapped, urlReplacer);
+		}
+
+		const archive = new jszip();
+		const pagePromises = [];
+		for (let pageNum=1; pageNum < numberOfPages; pageNum++){
+			pagePromises.push(thumbnailCapture(pageNum, brewRenderer));
+		}
+
+		Promise.all(pagePromises).then((resolveds)=>{
+			for (let pageNum=1; pageNum < numberOfPages; pageNum++){
+				const fileName = `${pageNum.toString().padStart(4, '0')}.Page-${pageNum == 0 ? 'Cover' : pageNum.toString()}.jpg`;
+				const pageBlob = resolveds[pageNum - 1];
+				archive.file(fileName, pageBlob);
+			}
+			archive.generateAsync({ type: 'blob' }).then((content)=>{
+				fileSaver.saveAs(content, `${document.title.slice(0, -1 * ' - The Homebrewery'.length).trim()}.cbz`);
+			});
+		});
+	}
+};
+
 module.exports = {
 	splitTextStyleAndMetadata,
-	printCurrentBrew
+	printCurrentBrew,
+	createBrewCBZ
 };
