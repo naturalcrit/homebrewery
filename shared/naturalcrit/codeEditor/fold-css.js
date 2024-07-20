9a4cc5f63eb7e71074ed5692127ace4bfd795889
G.Ambatte <sean@robertson-family.nz>
2024-07-20 14:57:09 +1200

    Add @import folding

--- shared/naturalcrit/codeEditor/fold-css.js
+++ shared/naturalcrit/codeEditor/fold-css.js
@@ -1,13 +1,14 @@
 module.exports = {
 	registerHomebreweryHelper : function(CodeMirror) {
 		CodeMirror.registerHelper('fold', 'homebrewerycss', function(cm, start) {
+
+			// BRACE FOLDING
 			const startMatcher = /\{[ \t]*$/;
 			const endMatcher = /\}[ \t]*$/;
 			const prevLine = cm.getLine(start.line);
 
-			if((start.line === cm.firstLine()) && (!cm.getLine(start.line).match(startMatcher))) return null;
 
-			if(start.line === cm.firstLine() || prevLine.match(startMatcher)) {
+			if(prevLine.match(startMatcher)) {
 				const lastLineNo = cm.lastLine();
 				let end = start.line + 1;
 				let braceCount = 1;
@@ -23,6 +24,17 @@ module.exports = {
 				return {
 					from : CodeMirror.Pos(start.line, 0),
 					to   : CodeMirror.Pos(end, cm.getLine(end).length)
+				};
+			}
+
+			// IMPORT FOLDING
+
+			const importMatcher = /^@import.*?[;]/;
+
+			if(prevLine.match(importMatcher)) {
+				return {
+					from : CodeMirror.Pos(start.line, 0),
+					to   : CodeMirror.Pos(start.line, cm.getLine(start.line).length)
 				};
 			}
 
