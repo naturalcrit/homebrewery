1631436ad6301b3416bae4ce0aadebd38347ced5
David Bolack <dbolack@gmail.com>
2024-07-30 03:31:16 -0500

    Restore missing separators.

--- shared/naturalcrit/markdown.js
+++ shared/naturalcrit/markdown.js
@@ -770,7 +770,7 @@ const processStyleMacros = (string, lastToken)=>{
 
 	if(twMatch) {
 		substitutions = substitutions.replace(twMatch[0],
-			`float:${twMatch[1] == 'twtl' ? 'left' : 'right'},shape-outside:url(${lastToken.href})${twMatch[3]?.length > 0 ? `shape-image-threshold:${twMatch[3]}` : ''}`);
+			`float:${twMatch[1] == 'twtl' ? 'left' : 'right'},shape-outside:url(${lastToken.href})${twMatch[3]?.length > 0 ? `; shape-image-threshold:${twMatch[3]}` : ''}`);
 	}
 
 	const textWrapMarginRegex = /\$(tw[lr])(-(\d*\.?\d+(%|\w{2,4})))?/;
@@ -778,7 +778,7 @@ const processStyleMacros = (string, lastToken)=>{
 
 	if(twMatch) {
 		substitutions = substitutions.replace(twMatch[0],
-			`float:${twMatch[1] == 'twl' ? 'left' : 'right'},shape-outside:url(${lastToken.href})${twMatch[3]?.length > 0 ? `shape-margin:${twMatch[3]}` : ''}`);
+			`float:${twMatch[1] == 'twl' ? 'left' : 'right'},shape-outside:url(${lastToken.href})${twMatch[3]?.length > 0 ? `; shape-margin:${twMatch[3]}` : ''}`);
 	}
 
 	return substitutions;
