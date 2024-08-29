f00c9633edb7ebc41d690b28cda63804744a0370
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2024-08-29 10:51:34 +0200

    simplify console log

--- server/app.js
+++ server/app.js
@@ -440,10 +440,7 @@ app.get('/account', asyncHandler(async (req, res, next)=>{
 
 const nodeEnv = config.get('node_env');
 
-console.group();
-console.log('Config vars')
-console.table(config);
-console.groupEnd();
+console.log('Config vars: ',config);
 
 const isLocalEnvironment = config.get('local_environments').includes(nodeEnv);
 // Local only
