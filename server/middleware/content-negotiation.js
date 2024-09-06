d19aaf6c780a8a8584ca4af5bbcc35bd5cdf8df1
David Bolack <dbolack@gmail.com>
2024-09-06 11:50:46 -0500

    Except staticImages and staticFonts paths from middleware evaluation if in a local ENV.

--- server/middleware/content-negotiation.js
+++ server/middleware/content-negotiation.js
@@ -1,12 +1,17 @@
-module.exports = (req, res, next)=>{
-	const isImageRequest = req.get('Accept')?.split(',')
-        ?.filter((h)=>!h.includes('q='))
-        ?.every((h)=>/image\/.*/.test(h));
-	if(isImageRequest) {
-		return res.status(406).send({
-			message : 'Request for image at this URL is not supported'
-		});
-	}
+const config = require('../config.js');
+const nodeEnv = config.get('node_env');
+const isLocalEnvironment = config.get('local_environments').includes(nodeEnv);
 
+module.exports = (req, res, next)=>{
+	if((!isLocalEnvironment) && (!req.url?.startsWith('/staticImages') && !req.url?.startsWith('/staticFonts'))) {
+		const isImageRequest = req.get('Accept')?.split(',')
+			?.filter((h)=>!h.includes('q='))
+			?.every((h)=>/image\/.*/.test(h));
+		if(isImageRequest) {
+			return res.status(406).send({
+				message : 'Request for image at this URL is not supported'
+			});
+		}
+	}
 	next();
 };
