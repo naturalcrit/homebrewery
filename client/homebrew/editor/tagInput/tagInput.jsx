d505e4e24c228d94467f5c99d1af90c973670343
Gazook89 <John.W.Jones89@gmail.com>
2024-09-17 23:16:06 -0500

    Render element for each value from props
    Take an array of values from props, load it into valueContext state with an "editing" boolean for each value.  Then, when rendering the component, take each value in the valueContext array and create a div for each --
    
    at this point, if the value is "being edited", it returns a div with text "editing".  If not being edited, it returns a div with the value as text.
    
    Nothing is being edited at this point since that functionality doesn't exist yet.

--- client/homebrew/editor/tagInput/tagInput.jsx
+++ client/homebrew/editor/tagInput/tagInput.jsx
@@ -1,19 +1,29 @@
 const React = require('react');
-const { useState, useRef, useEffect } = React;
+const { useState } = React;
 const _     = require('lodash');
 
-const TagInput = ({unique, ...props})=>{
+const TagInput = ({unique = true, values = [], ...props})=>{
 
 	const [temporaryValue, setTemporaryValue] = useState('');
+	const [valueContext, setValueContext] = useState(values.map((value)=>({ value: value, editing : false })));
+
+	const tagElement = (value)=>{
+		return (
+			<div className={`badge`}>{value}</div>
+		)
+	}
 
 	return (
-		<div className='field tag-input'>
-			<label>{props.label}
-				<input type='text'
-					className={`value`}
-					placeholder={props.placeholder}
-					value={temporaryValue} />
-			</label>
+		<div className='field'>
+
+			<label>{props.label}</label>
+			{Object.values(valueContext).map((context, i)=>{ return context.editing ? tagElement('editing') : tagElement(context.value) })}
+
+			<input type='text'
+				className={`value`}
+				placeholder={props.placeholder}
+				value={temporaryValue}
+				onChange={(e)=>setTemporaryValue(e.target.value)} />
 		</div>
 	)
 }
