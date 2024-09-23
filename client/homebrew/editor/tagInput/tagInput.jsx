bfc0f5d73acc540b80b78994c15fa16328cad030
Gazook89 <John.W.Jones89@gmail.com>
2024-09-22 20:17:46 -0500

    Improve keyboard navigation
    Add options for:
    
    - arrow key and tab navigation,
    - 'Enter' or 'Space' to edit a tag
    - 'Enter', 'Tab', or ',' to submit a new tag
    - 'Escape' to exit a tag without submitting
    - 'Delete' to delete a tag
    
    Tabbing from outside the component sends focus to the new tag input, from which you can shift-tab or arrowLeft to move focus through the tags.
    
    Tabbing or shift-tabbing through the last tag or past the new tag input sends focus out of the component to wherever the next focus point is.

--- client/homebrew/editor/tagInput/tagInput.jsx
+++ client/homebrew/editor/tagInput/tagInput.jsx
@@ -1,57 +1,79 @@
 require('./tagInput.less');
 const React = require('react');
-const { useState, useEffect } = React;
-const _     = require('lodash');
+const { useState, useEffect, useRef } = React;
+const _ = require('lodash');
 
 const TagInput = ({ unique = true, values = [], ...props }) => {
 	const [temporaryValue, setTemporaryValue] = useState('');
-	const [valueContext, setValueContext] = useState(values.map((value) => ({ value, editing: false })));
 	const [focusedIndex, setFocusedIndex] = useState(-1);
+	const [valueContext, setValueContext] = useState(values.map((value) => ({ value, editing: false })));
+	const tagRefs = useRef([]);
 
-	useEffect(()=>{
-		handleChange(valueContext.map((context)=>context.value))
-	}, [valueContext])
+	useEffect(() => {
+		handleChange(valueContext.map((context) => context.value));
+		tagRefs.current = tagRefs.current.slice(0, valueContext.length);
+	}, [valueContext]);
 
-	const handleChange = (value)=>{
+	useEffect(() => {
+		if (focusedIndex >= 0 && focusedIndex < tagRefs.current.length) {
+			tagRefs.current[focusedIndex]?.focus();
+		}
+	}, [valueContext, focusedIndex]);
+
+	const handleChange = (value) => {
 		props.onChange({
-			target : { value }
-		})
+			target: { value }
+		});
 	};
 
 	const handleInputKeyDown = ({ evt, value, index = valueContext.length, options = {} }) => {
-		if (_.includes(['Enter', ','], evt.key) && valueContext[index].editing == true) {
-			evt.preventDefault();
-			submitTag(evt.target.value, value, index);
+		if (_.includes(['Enter', ','], evt.key)) {
+			if (!valueContext[index]) {
+				submitTag(evt.target.value, null, null, evt);
+			} else if (valueContext[index].editing === true) {
+				submitTag(evt.target.value, value, index, evt);
+			} else if (evt.key === 'Enter' && valueContext[index].editing === false) {
+				editTag(index);
+			}
 			if (options.clear) {
 				setTemporaryValue('');
 			}
-		} else if(evt.key === 'Escape'){
-			submitTag(value, value, index)
-		} else if(evt.key === 'Delete') {
-			submitTag(null, null, index);
-		} else if(evt.key === 'Tab' && evt.shiftKey){
-			setFocus(index - 1);
-		} else if(evt.key === 'Tab'){
-			setFocus(index + 1);
+		} else if (evt.key === ' ' && valueContext[index].editing === false) {
+			evt.preventDefault();
+			editTag(index);
+		} else if (evt.key === 'Escape') {
+			submitTag(value, value, index, evt);
+		} else if (evt.key === 'Delete') {
+			submitTag(null, null, index, evt);
+		} else if ((evt.key === 'Tab' && evt.shiftKey) || evt.key === 'ArrowLeft') {
+			setFocus(index - 1, evt);
+		} else if (evt.key === 'Tab' || evt.key === 'ArrowRight') {
+			setFocus(index + 1, evt);
 		}
 	};
 
-	const setFocus = (index)=>{
-		if (index < 0 || index >= valueContext.length){ setFocusedIndex(-1); return};
+	const setFocus = (index, evt) => {
+		if (index < 0 || index >= valueContext.length) {
+			setFocusedIndex(-1);
+			evt.target.blur();
+			return;
+		}
+		evt.preventDefault();
 		setFocusedIndex(index);
-	}
+	};
 
-	const submitTag = (newValue, originalValue, index) => {
+	const submitTag = (newValue, originalValue, index, evt) => {
+		evt.preventDefault();
 		setValueContext((prevContext) => {
-			// remove existing tag
-			if(newValue === null){
-				return [...prevContext].filter((context, i)=>i !== index);
+			// Remove tag
+			if (newValue === null) {
+				return [...prevContext].filter((context, i) => i !== index);
 			}
-			// add new tag
-			if(originalValue === null){
-				return [...prevContext, { value: newValue, editing: false }]
+			// Add tag
+			if (originalValue === null) {
+				return [...prevContext, { value: newValue, editing: false }];
 			}
-			// update existing tag
+			// Update tag
 			return prevContext.map((context, i) => {
 				if (i === index) {
 					return { ...context, value: newValue, editing: false };
@@ -74,25 +96,40 @@ const TagInput = ({ unique = true, values = [], ...props }) => {
 
 	const renderReadTag = (context, index) => {
 		return (
-			<li key={index}
+			<li
+				key={index}
+				ref={(el) => (tagRefs.current[index] = el)}
 				data-value={context.value}
 				className={`tag${focusedIndex === index ? ' focused' : ''}`}
-				tabIndex={focusedIndex === index ? 0 : -1}
 				onClick={() => editTag(index)}
-				onKeyDown={(evt)=>handleInputKeyDown({evt, index: index})}>
+				onKeyDown={(evt) => handleInputKeyDown({ evt, index })}
+				tabIndex={focusedIndex === index ? 0 : -1}
+				onFocus={() => setFocusedIndex(index)}
+			>
 				{context.value}
-				<button tabIndex={-1} onClick={(evt)=>{evt.stopPropagation(); submitTag(null, context.value, index)}}><i className='fa fa-times fa-fw'/></button>
+				<button
+					tabIndex={-1}
+					onClick={(evt) => {
+						evt.stopPropagation();
+						submitTag(null, context.value, index, evt);
+					}}
+				>
+					<i className='fa fa-times fa-fw' />
+				</button>
 			</li>
 		);
 	};
 
 	const renderWriteTag = (context, index) => {
 		return (
-			<input type='text'
+			<input
+				type='text'
+				ref={(el) => (tagRefs.current[index] = el)}
 				key={index}
-				defaultValue={context.value} 
-				onKeyDown={(evt) => handleInputKeyDown({evt, value: context.value, index: index})} 
-				autoFocus 
+				defaultValue={context.value}
+				tabIndex={focusedIndex === index ? 0 : -1}
+				onKeyDown={(evt) => handleInputKeyDown({ evt, value: context.value, index })}
+				autoFocus
 			/>
 		);
 	};
@@ -102,7 +139,9 @@ const TagInput = ({ unique = true, values = [], ...props }) => {
 			<label>{props.label}</label>
 			<div className='value'>
 				<ul className='list'>
-					{valueContext.map((context, index) => { return context.editing ? renderWriteTag(context, index) : renderReadTag(context, index); })}
+					{valueContext.map((context, index) => {
+						return context.editing ? renderWriteTag(context, index) : renderReadTag(context, index);
+					})}
 				</ul>
 
 				<input
