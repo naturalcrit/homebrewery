1fff75cc5e52c56144cd90a5f0918f3b920d8142
Trevor Buckner <calculuschild@gmail.com>
2024-10-11 23:42:20 -0400

    Update homebrew.api.js

--- server/homebrew.api.js
+++ server/homebrew.api.js
@@ -267,7 +267,7 @@ const api = {
 	getThemeBundle : async(req, res)=>{
 		/*	getThemeBundle: Collects the theme and all parent themes
 				returns an object containing an array of css, and an array of snippets, in render order
-    
+
 				req.params.id       : The shareId ( User theme ) or name ( static theme )
 				req.params.renderer : The Markdown renderer used for this theme */
 
