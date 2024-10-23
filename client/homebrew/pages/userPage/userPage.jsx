b9bf9c7e70fe6704e9cb4ce3a4fd4eb2cfd8d764
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2024-10-23 17:03:00 +0200

    "Refactor UserPage component: removed unnecessary import, updated function signature, and moved useState hook declaration"

--- client/homebrew/pages/userPage/userPage.jsx
+++ client/homebrew/pages/userPage/userPage.jsx
@@ -1,12 +1,11 @@
 const React = require('react');
-const { useState, useEffect } = require('react');
+const { useState } = React;
 const _ = require('lodash');
 
 const ListPage = require('../basePages/listPage/listPage.jsx');
 
 const Nav = require('naturalcrit/nav/nav.jsx');
 const Navbar = require('../../navbar/navbar.jsx');
-
 const RecentNavItem = require('../../navbar/recent.navitem.jsx').both;
 const Account = require('../../navbar/account.navitem.jsx');
 const NewBrew = require('../../navbar/newbrew.navitem.jsx');
@@ -14,13 +13,12 @@ const HelpNavItem = require('../../navbar/help.navitem.jsx');
 const ErrorNavItem = require('../../navbar/error-navitem.jsx');
 const VaultNavitem = require('../../navbar/vault.navitem.jsx');
 
-const UserPage = (props) => {
-    const { username = '', brews = [], query = '', error = null } = props;
-    const usernameWithS = username + (username.endsWith('s') ? `’` : `’s`);
+const UserPage = ({ username = '', brews = [], query = '', error = null, ...props }) => {
 
-    const groupedBrews = _.groupBy(brews, (brew) => {
-        return brew.published ? 'published' : 'private';
-    });
+    const [currentError, setCurrentError] = useState(error);
+
+    const usernameWithS = username + (username.endsWith('s') ? `’` : `’s`);
+    const groupedBrews = _.groupBy(brews, brew => brew.published ? 'published' : 'private');
 
 	const brewCollection = [
 		{
@@ -34,12 +32,6 @@ const UserPage = (props) => {
 			brews: groupedBrews.private || []
 		}] : [])
 	];	
-
-    const [currentError, setCurrentError] = useState(error);
-
-    const errorReported = (error) => {
-        setCurrentError(error);
-    };
 
     const navItems = () => (
         <Navbar>
@@ -55,8 +47,7 @@ const UserPage = (props) => {
     );
 
     return (
-        <ListPage brewCollection={brewCollection}  navItems={navItems()} query={query} reportError={errorReported}
-        />
+        <ListPage brewCollection={brewCollection}  navItems={navItems()} query={query} reportError={(error) =>setCurrentError(error)} />
     );
 };
 
