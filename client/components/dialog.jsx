b7b1981bdefcab631ea57de20a9a3c5fe8f75190
Gazook89 <John.W.Jones89@gmail.com>
2024-11-08 21:56:45 -0600

    lint

--- client/components/dialog.jsx
+++ client/components/dialog.jsx
@@ -1,25 +1,25 @@
 // Dialog box, for popups and modal blocking messages
-import React from "react";
+import React from 'react';
 const { useRef, useEffect } = React;
 
 function Dialog({ dismisskeys = [], closeText = 'Close', blocking = false, ...rest }) {
 	const dialogRef = useRef(null);
 
 	useEffect(()=>{
-		if (dismisskeys.length !== 0) {
+		if(dismisskeys.length !== 0) {
 			blocking ? dialogRef.current?.showModal() : dialogRef.current?.show();
 		}
 	}, [dialogRef.current, dismisskeys]);
 
-	const dismiss = () => {
-		dismisskeys.forEach(key => {
-			if (key) {
+	const dismiss = ()=>{
+		dismisskeys.forEach((key)=>{
+			if(key) {
 				localStorage.setItem(key, 'true');
 			}
 		});
 		dialogRef.current?.close();
 	};
-	
+
 	return (
 		<dialog ref={dialogRef} onCancel={dismiss} {...rest}>
 			{rest.children}
