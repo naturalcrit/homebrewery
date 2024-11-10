dc1d40512b9c1aed90ee4b69dd464f214d5bc62f
G.Ambatte <sean@robertson-family.nz>
2024-11-10 21:45:17 +1300

    Reinstate length check

--- client/admin/brewUtils/brewLookup/brewLookup.jsx
+++ client/admin/brewUtils/brewLookup/brewLookup.jsx
@@ -81,7 +81,7 @@ const BrewLookup = createClass({
 				<dt>Number of SCRIPT tags detected</dt>
 				<dd>{this.state.scriptCount}</dd>
 			</dl>
-			{this.state.scriptCount &&
+			{this.state.scriptCount > 0 &&
 				<div className='cleanButton'>
 					<button onClick={this.cleanScript}>CLEAN BREW</button>
 				</div>
