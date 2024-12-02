9f9948f5315aa7334aba0c0cb53b3382f3997a2e
Trevor Buckner <calculuschild@gmail.com>
2024-12-02 12:23:54 -0500

    Add comment

--- shared/helpers.js
+++ shared/helpers.js
@@ -22,6 +22,7 @@ const splitTextStyleAndMetadata = (brew)=>{
 		brew.text = brew.text.slice(index + 5);
 	}
 
+	// Handle old brews that still have empty strings in the tags metadata
 	if(typeof brew.tags === 'string') brew.tags = brew.tags ? [brew.tags] : [];
 };
 
