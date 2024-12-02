e17db0788c0b07ff03cdcbd166ae7f3503b28993
G.Ambatte <sean@robertson-family.nz>
2024-12-02 16:18:18 +1300

    Convert any `tags` strings to arrays

--- shared/helpers.js
+++ shared/helpers.js
@@ -21,6 +21,8 @@ const splitTextStyleAndMetadata = (brew)=>{
 		brew.snippets = brew.text.slice(11, index - 1);
 		brew.text = brew.text.slice(index + 5);
 	}
+
+	if(typeof brew.tags === 'string') brew.tags = brew.tags ? [brew.tags] : [];
 };
 
 const printCurrentBrew = ()=>{
