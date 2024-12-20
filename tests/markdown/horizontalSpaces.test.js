6e9d293bbe33b900217795a03ea341f1681c6f85
Trevor Buckner <calculuschild@gmail.com>
2024-12-19 23:09:43 -0500

    Rename tests to "Non-breaking Spaces"
    Hard Breaks name was leftover from copying the `::::` test file.

--- tests/markdown/horizontalSpaces.test.js
+++ tests/markdown/horizontalSpaces.test.js
@@ -2,44 +2,44 @@
 
 import Markdown from 'naturalcrit/markdown.js';
 
-describe('Hard Breaks', ()=>{
-	test('Single Break', function() {
+describe('Non-Breaking Spaces', ()=>{
+	test('Single Space', function() {
 		const source = ':>\n\n';
 		const rendered = Markdown.render(source).trim();
 		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe(`<p>&nbsp;</p>`);
 	});
 
-	test('Double Break', function() {
+	test('Double Space', function() {
 		const source = ':>>\n\n';
 		const rendered = Markdown.render(source).trim();
 		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe(`<p>&nbsp;&nbsp;</p>`);
 	});
 
-	test('Triple Break', function() {
+	test('Triple Space', function() {
 		const source = ':>>>\n\n';
 		const rendered = Markdown.render(source).trim();
 		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe(`<p>&nbsp;&nbsp;&nbsp;</p>`);
 	});
 
-	test('Many Break', function() {
+	test('Many Space', function() {
 		const source = ':>>>>>>>>>>\n\n';
 		const rendered = Markdown.render(source).trim();
 		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe(`<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>`);
 	});
 
-	test('Multiple sets of Breaks', function() {
+	test('Multiple sets of Spaces', function() {
 		const source = ':>>>\n:>>>\n:>>>';
 		const rendered = Markdown.render(source).trim();
 		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe(`<p>&nbsp;&nbsp;&nbsp;\n&nbsp;&nbsp;&nbsp;\n&nbsp;&nbsp;&nbsp;</p>`);
 	});
 
-	test('Pair of inline Breaks', function() {
+	test('Pair of inline Spaces', function() {
 		const source = ':>>:>>';
 		const rendered = Markdown.render(source).trim();
 		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe(`<p>&nbsp;&nbsp;&nbsp;&nbsp;</p>`);
 	});
 
-	test('Break directly between two paragraphs', function() {
+	test('Space directly between two paragraphs', function() {
 		const source = 'Line 1\n:>>\nLine 2';
 		const rendered = Markdown.render(source).trim();
 		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe(`<p>Line 1\n&nbsp;&nbsp;\nLine 2</p>`);
@@ -58,7 +58,7 @@ describe('Hard Breaks', ()=>{
 		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe(`<dl><dt>Term</dt>\n<dd>> Definition 1</dd></dl>`);
 	});
 
-	test('I am actually a two term definition list!', function() {
+	test('I am actually a two-term definition list!', function() {
 		const source = 'Term\n::> Definition 1\n::>> Definition 2';
 		const rendered = Markdown.render(source).trim();
 		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe(`<dl><dt>Term</dt>\n<dd>> Definition 1</dd>\n<dd>>> Definition 2</dd></dl>`);
