1aeea034d2c54939a847fd462faf84e748b27b9b
David Bolack <dbolack@gmail.com>
2025-01-09 22:34:35 -0600

    Requested corrections?

--- client/homebrew/pages/sharePage/sharePage.jsx
+++ client/homebrew/pages/sharePage/sharePage.jsx
@@ -23,7 +23,9 @@ const SharePage = (props)=>{
 	});
 
 	const handleBrewRendererPageChange = useCallback((pageNumber)=>{
-		this.setState({ currentBrewRendererPageNum: pageNumber });
+		setState((prevState)=>({
+			currentBrewRendererPageNum : pageNumber,
+			...prevState }));
 	}, []);
 
 	const handleControlKeys = (e)=>{
