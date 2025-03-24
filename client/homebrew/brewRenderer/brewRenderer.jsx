0fc7571c35940bd5252a0f190f2a5767672a46e9
David Bolack <dbolack@gmail.com>
2025-03-23 20:41:14 -0500

    Somehow this change did not get commited or pushed.
    This swaps the if block for an optional chaining

--- client/homebrew/brewRenderer/brewRenderer.jsx
+++ client/homebrew/brewRenderer/brewRenderer.jsx
@@ -186,14 +186,12 @@ const BrewRenderer = (props)=>{
 		} else {
 			if(pageText.startsWith('\\page')) {
 				const firstLineTokens  = Markdown.marked.lexer(pageText.split('\n', 1)[0])[0].tokens;
-				if(firstLineTokens) { 		// Catch invalid/empty mustache blocks
-					const injectedTags = firstLineTokens.find((obj)=>obj.injectedTags !== undefined)?.injectedTags;
-					if(injectedTags) {
-						styles     = { ...styles, ...injectedTags.styles };
-						styles     = _.mapKeys(styles, (v, k)=>k.startsWith('--') ? k : _.camelCase(k)); // Convert CSS to camelCase for React
-						classes    = [classes, injectedTags.classes].join(' ').trim();
-						attributes = injectedTags.attributes;
-					}
+				const injectedTags = firstLineTokens?.find((obj)=>obj.injectedTags !== undefined)?.injectedTags;
+				if(injectedTags) {
+					styles     = { ...styles, ...injectedTags.styles };
+					styles     = _.mapKeys(styles, (v, k)=>k.startsWith('--') ? k : _.camelCase(k)); // Convert CSS to camelCase for React
+					classes    = [classes, injectedTags.classes].join(' ').trim();
+					attributes = injectedTags.attributes;
 				}
 				pageText = pageText.includes('\n') ? pageText.substring(pageText.indexOf('\n') + 1) : ''; // Remove the \page line
 			}
