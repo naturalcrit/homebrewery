e2f2b2962f47532df8079e2294718f0b62a0cc27
G.Ambatte <sean@robertson-family.nz>
2025-04-07 17:34:31 +1200

    Revert request middleware change as it is no longer necessary

--- client/homebrew/utils/request-middleware.js
+++ client/homebrew/utils/request-middleware.js
@@ -1,8 +1,6 @@
-import packageJSON from '../../../package.json' with { type: 'json' };
 import request from 'superagent';
 
-
-const addHeader = (request)=>request.set('Homebrewery-Version', packageJSON.version);
+const addHeader = (request)=>request.set('Homebrewery-Version', global.version);
 
 const requestMiddleware = {
 	get    : (path)=>addHeader(request.get(path)),
