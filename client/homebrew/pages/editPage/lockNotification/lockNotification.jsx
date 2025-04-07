4c4a023f34305073f8677331aca0fe6e475ac7a6
G.Ambatte <sean@robertson-family.nz>
2025-04-07 15:59:06 +1200

    Fix lock notification message

--- client/homebrew/pages/editPage/lockNotification/lockNotification.jsx
+++ client/homebrew/pages/editPage/lockNotification/lockNotification.jsx
@@ -8,6 +8,7 @@ function LockNotification(props) {
 		shareId         : 0,
 		disableLock     : ()=>{},
 		lock            : {},
+		message         : 'Unable to retrieve Lock Message',
 		reviewRequested : false,
 		...props
 	};
@@ -31,7 +32,7 @@ function LockNotification(props) {
 		<p>This brew been locked by the Administrators. It will not be accessible by any method other than the Editor until the lock is removed.</p>
 		<hr />
 		<h3>LOCK REASON</h3>
-		<p>{props.lock.editMessage || 'Unable to retrieve Lock Message'}</p>
+		<p>{props.message}</p>
 		<hr />
 		<p>Once you have resolved this issue, click REQUEST LOCK REMOVAL to notify the Administrators for review.</p>
 		<p>Click CONTINUE TO EDITOR to temporarily hide this notification; it will reappear the next time the page is reloaded.</p>
