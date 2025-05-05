e15c6877e666dbdccb4981f02116f87f357ff441
Gazook89 <John.W.Jones89@gmail.com>
2025-05-05 18:50:37 -0500

    remove or rearrange extraneous navbar item files

--- client/homebrew/navbar/mainMenu.navitem.jsx
+++ client/homebrew/navbar/mainMenu.navitem.jsx
@@ -1,15 +1,8 @@
 require('./navbar.less');
 const React = require('react');
-
+const dedent = require('dedent-tabs').default;
 
 const { MenuDropdown, MenuItem } = require('../../components/menubar/Menubar.jsx');
-const HelpNavItems = require('./help.navitem.jsx');
-const PatreonNavItem = require('./patreon.navitem.jsx');
-
-
-
-
-
 const NaturalCritIcon = require('naturalcrit/svg/naturalcrit.svg.jsx');
 
 const MainMenu = () => {
@@ -24,8 +17,36 @@ const MainMenu = () => {
 			<MenuItem newTab={true} href='/changelog' color='purple' icon='far fa-file-alt'>
 				{`v${global.version}`}
 			</MenuItem>
-			<HelpNavItems />
-			<PatreonNavItem />
+			<MenuItem color='red' icon='fas fa-fw fa-bug'
+				href={`https://www.reddit.com/r/homebrewery/submit?selftext=true&text=${encodeURIComponent(dedent`
+				- **Browser(s)** :
+				- **Operating System** :  
+				- **Legacy or v3 Renderer** :
+				- **Issue** :  `)}`}
+				newTab={true}
+				rel='noopener noreferrer'>
+				report to reddit
+			</MenuItem>
+			<MenuItem color='green'
+				href='/faq'
+				newTab={true}
+				rel='noopener noreferrer'>
+				FAQ
+			</MenuItem>
+			<MenuItem color='blue'
+				href='/migrate'
+				newTab={true}
+				rel='noopener noreferrer'>
+				migrate
+			</MenuItem>
+			<MenuItem
+				className='patreon'
+				newTab={true}
+				href='https://www.patreon.com/NaturalCrit'
+				color='green'
+				icon='fas fa-heart'>
+				Patreon
+			</MenuItem>
 		</MenuDropdown>
 	)
 };
