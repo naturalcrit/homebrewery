7e2cb09d8688f13bd878d99009331cadf8e9c520
Gazook89 <John.W.Jones89@gmail.com>
2025-05-27 00:11:46 -0500

    flip the divider tab to the left side if on right edge

--- shared/naturalcrit/splitPane/splitPane.jsx
+++ shared/naturalcrit/splitPane/splitPane.jsx
@@ -19,17 +19,24 @@ const SplitPane = (props)=>{
 	const [dragStartTime, setDragStartTime] = useState(0);
   	const [dragStartPos, setDragStartPos] = useState(0);
   	const [lastWidth, setLastWidth] = useState(null);
+	const [windowWidth, setWindowWidth] = useState(0);
 
 	useEffect(()=>{
 		const savedPos = window.localStorage.getItem(storageKey);
-		setDividerPos(savedPos ? limitPosition(savedPos, 0.1 * (window.innerWidth - 13), 0.9 * (window.innerWidth - 13)) : window.innerWidth / 2);
+		setDividerPos(savedPos ? limitPosition(savedPos, 0.1 * (window.innerWidth - 4), 0.9 * (window.innerWidth - 13)) : window.innerWidth / 2);
 		setLiveScroll(window.localStorage.getItem('liveScroll') === 'true');
+		setWindowWidth(window.innerWidth);
 
-		window.addEventListener('resize', handleResize);
-		return ()=>window.removeEventListener('resize', handleResize);
+		const handleWindowResize = () => {
+			setWindowWidth(window.innerWidth);
+			handleResize();
+		};
+
+		window.addEventListener('resize', handleWindowResize);
+		return () => window.removeEventListener('resize', handleWindowResize);
 	}, []);
 
-	const limitPosition = (x, min = 1, max = window.innerWidth - 13)=>Math.round(Math.min(max, Math.max(min, x)));
+	const limitPosition = (x, min = 1, max = window.innerWidth - 4)=>Math.round(Math.min(max, Math.max(min, x)));
 
 	//when resizing, the divider should grow smaller if less space is given, then grow back if the space is restored, to the original position
 	const handleResize = ()=>setDividerPos(limitPosition(window.localStorage.getItem(storageKey), 0.1 * (window.innerWidth - 13), 0.9 * (window.innerWidth - 13)));
@@ -59,6 +66,7 @@ const SplitPane = (props)=>{
 
 	const handleDown = (e)=>{
 		e.preventDefault();
+    	e.target.setPointerCapture(e.pointerId);
 		setIsDragging(true);
 		setDragStartTime(Date.now());
 		setDragStartPos(e.pageX);
@@ -74,6 +82,10 @@ const SplitPane = (props)=>{
 		window.localStorage.setItem('liveScroll', String(!liveScroll));
 		setLiveScroll(!liveScroll);
 	};
+
+	const onRightEdge = ()=>{
+		return dividerPos > (windowWidth - 16);
+	}
 
 	const  renderMoveArrows = (showMoveArrows &&
 		<>
@@ -95,7 +107,7 @@ const SplitPane = (props)=>{
 	const renderDivider = (
 		<div className={`divider ${isDragging && 'dragging'}`} onPointerDown={handleDown}>
 			{showDividerButtons && renderMoveArrows}
-			<div className='dots'>
+			<div className={`dots${onRightEdge() ? ' on-right-edge' : ''}`}>
 				<i className='fas fa-circle' />
 				<i className='fas fa-circle' />
 				<i className='fas fa-circle' />
