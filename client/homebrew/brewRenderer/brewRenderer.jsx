3626ed5a31ebb9b0f7a6390cc7ffd6017fa793aa
Trevor Buckner <calculuschild@gmail.com>
2025-07-15 14:47:04 -0400

    Rename regex, move column replacement
    Renaming COLUMNBREAK_REGEX_LEGACY for consistency in naming scheme with the other regexes.
    
    Moving the legacy `\column` replacement down to `renderPages()` where we do similar text modification steps for V3.

--- client/homebrew/brewRenderer/brewRenderer.jsx
+++ client/homebrew/brewRenderer/brewRenderer.jsx
@@ -21,7 +21,7 @@ import { safeHTML } from './safeHTML.js';
 
 const PAGEBREAK_REGEX_V3 = /^(?=\\page(?:break)?(?: *{[^\n{}]*})?$)/m;
 const PAGEBREAK_REGEX_LEGACY = /\\page(?:break)?/m;
-const COLUMNBREAK_LEGACY = /\\column(:?break)?/m;
+const COLUMNBREAK_REGEX_LEGACY = /\\column(:?break)?/m;
 const PAGE_HEIGHT = 1056;
 
 const INITIAL_CONTENT = dedent`
@@ -132,7 +132,7 @@ const BrewRenderer = (props)=>{
 	const pagesRef = useRef(null);
 
 	if(props.renderer == 'legacy') {
-		rawPages = props.text.replace(COLUMNBREAK_LEGACY, '```\n````\n').split(PAGEBREAK_REGEX_LEGACY);
+		rawPages = props.text.split(PAGEBREAK_REGEX_LEGACY);
 	} else {
 		rawPages = props.text.split(PAGEBREAK_REGEX_V3);
 	}
@@ -189,6 +189,7 @@ const BrewRenderer = (props)=>{
 		let attributes = {};
 
 		if(props.renderer == 'legacy') {
+			pageText.replace(COLUMNBREAK_REGEX_LEGACY, '```\n````\n'); // Allow Legacy brews to use `\column(break)`
 			const html = MarkdownLegacy.render(pageText);
 
 			return <BrewPage className='page phb' index={index} key={index} contents={html} style={styles} onVisibilityChange={handlePageVisibilityChange} />;
