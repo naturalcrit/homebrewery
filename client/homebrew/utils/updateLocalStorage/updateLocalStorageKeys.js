1eb5b6d3a4948ecce5eb3b26129df25c872b1d7a
G.Ambatte <sean@robertson-family.nz>
2025-09-09 22:04:40 +1200

    Copy exisitng key data to new keys

---
+++ client/homebrew/utils/updateLocalStorage/updateLocalStorageKeys.js
@@ -0,0 +1,22 @@
+import localStorageKeyMap from './localStorageKeyMap.json' with { type: 'json' };
+
+const updateLocalStorage = function(){
+	// Return if no window and thus no local storage
+	if(typeof window === 'undefined') return;
+
+	// Return if the local storage key map has no content
+	if(Object.keys(localStorageKeyMap).length == 0) return;
+
+	const storage = window.localStorage;
+
+	Object.keys(localStorageKeyMap).forEach((key)=>{
+		if(storage[key]){
+			const data = storage.getItem(key);
+			storage.setItem(localStorageKeyMap[key], data);
+			// storage.removeItem(key);
+		}
+	});
+
+};
+
+export { updateLocalStorage };
