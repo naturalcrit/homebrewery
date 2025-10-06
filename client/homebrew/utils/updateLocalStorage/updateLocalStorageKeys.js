643e0ac6505765bae2de9117e48a23ad981bda00
Trevor Buckner <calculuschild@gmail.com>
2025-10-05 23:24:50 -0400

    small cleanups of localstorage keys code

--- client/homebrew/utils/updateLocalStorage/updateLocalStorageKeys.js
+++ client/homebrew/utils/updateLocalStorage/updateLocalStorageKeys.js
@@ -4,10 +4,7 @@ const updateLocalStorage = function(){
 	// Return if no window and thus no local storage
 	if(typeof window === 'undefined') return;
 
-	// Return if the local storage key map has no content
 	const localStorageKeyMap = getLocalStorageMap();
-	if(Object.keys(localStorageKeyMap).length == 0) return;
-
 	const storage = window.localStorage;
 
 	Object.keys(localStorageKeyMap).forEach((key)=>{
