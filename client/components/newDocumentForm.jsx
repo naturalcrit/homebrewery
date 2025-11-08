142fb8376fd9f36abc80ee41960ef933e3882788
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2025-11-09 00:48:37 +0100

    basic styles

--- client/components/newDocumentForm.jsx
+++ client/components/newDocumentForm.jsx
@@ -1,11 +1,35 @@
 import React from "react";
-
+import './newDocumentForm.less';
 
 export function NewDocumentForm() {
 
-  return (
-    <div>
-      Hello
+  return <>
+    <nav>
+      <button>templates</button>
+      <button>import</button>
+      <button>clone</button>
+    </nav>
+    <div className="content">
+<div className="templates">
+      <div className="template a4"><div className="image"></div><span>A4</span></div>
+      <div className="template a5"><div className="image"></div><span>A5</span></div>
+      <div className="template a4"><div className="image"></div><span>A4</span></div>
+      <div className="template a5"><div className="image"></div><span>A5</span></div>
+      <div className="template a4"><div className="image"></div><span>A4</span></div>
+      <div className="template a5"><div className="image"></div><span>A5</span></div>
     </div>
-  );
+    <div className="metadataPanel">
+      <div className="fieldGroup">
+        <label>title</label>
+						<input type='text'
+							defaultValue=''
+							placeholder='title'
+							className='title'
+							onChange={(e)=>{}} />
+      </div>
+    </div>
+    </div>
+    
+  </>
+
 }
