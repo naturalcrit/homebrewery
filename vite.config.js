9fe918e2f0a454d5a32078a8e2d9e3b5478755a6
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2026-01-20 19:39:28 +0100

    vite config

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+	plugins: [react()],
+	root: "client",
+	build: {
+		outDir: "../build",
+		emptyOutDir: true,
+		ssrManifest: true,
+		rollupOptions: {
+			input: {
+				admin: path.resolve(__dirname, "client/admin/admin.jsx"),
+				homebrew: path.resolve(__dirname, "client/homebrew/homebrew.jsx"),
+			},
+			output: {
+				entryFileNames: "[name]/bundle.js",
+				chunkFileNames: "[name]/[name]-[hash].js",
+				assetFileNames: "[name]/[name].[ext]",
+			},
+		},
+	},
+	server: {
+		fs: {
+			allow: ["."],
+		},
+	},
+});
