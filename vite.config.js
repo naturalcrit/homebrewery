efe67d20926bd9491c5b466e59e655d0220c5244
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2026-01-24 01:24:31 +0100

    update build folder end

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 export default defineConfig({
 	plugins: [react()],
 	build: {
-		outDir: "../build",
+		outDir: "build",
 		emptyOutDir: true,
 		ssrManifest: true,
 		rollupOptions: {
