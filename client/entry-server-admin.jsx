9dc2752ac3f2bdc51ebbb6f340318100bbc34bc9
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2026-02-01 17:47:37 +0100

    remove ssr only files

--- client/entry-server-admin.jsx
+++
@@ -1,4 +0,0 @@
-import { renderToString } from 'react-dom/server';
-import Admin from './admin/admin.jsx';
-
-export default (props) => renderToString(<Admin {...props} />);
--- client/entry-server-homebrew.jsx
+++
@@ -1,4 +0,0 @@
-import { renderToString } from 'react-dom/server';
-import Homebrew from './homebrew/homebrew.jsx';
-
-export default (props) => renderToString(<Homebrew {...props} />);
