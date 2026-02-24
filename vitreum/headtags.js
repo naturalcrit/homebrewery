a5919e627b79803207021404083a80134411d0a4
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2026-02-24 23:24:24 +0100

    join vitreum files

--- vitreum/headtags.js
+++ vitreum/headtags.js
@@ -1,5 +1,13 @@
 import React, { useEffect } from "react";
-import injectTag from "./injectTag.js";
+
+//old vitreum file, still imported in some pages
+
+const injectTag = (tag, props, children) => {
+  const injectNode = document.createElement(tag);
+  Object.entries(props).forEach(([key, val]) => injectNode[key] = val);
+  if (children) injectNode.appendChild(document.createTextNode(children));
+  document.getElementsByTagName('head')[0].appendChild(injectNode);
+};
 
 const obj2props = (obj) =>
 	Object.entries(obj)
--- vitreum/injectTag.js
+++
@@ -1,8 +0,0 @@
-const injectTag = (tag, props, children) => {
-  const injectNode = document.createElement(tag);
-  Object.entries(props).forEach(([key, val]) => injectNode[key] = val);
-  if (children) injectNode.appendChild(document.createTextNode(children));
-  document.getElementsByTagName('head')[0].appendChild(injectNode);
-};
-
-export default injectTag;
