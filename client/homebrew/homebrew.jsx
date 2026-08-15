50d58bbf4499f4e47f06ce9a61b8fe95438c66d9
Gazook89 <John.W.Jones89@gmail.com>
2026-08-15 13:00:18 -0500

    parse config object.
    This is for heroku which sends the config over as a string, not an object, so it has to be parsed into an object.

--- client/homebrew/homebrew.jsx
+++ client/homebrew/homebrew.jsx
@@ -48,13 +48,18 @@ const Homebrew = (props)=>{
 
 
 	const backgroundObject = ()=>{
-		if(config?.deployment || config?.local) {
-			const bgText = (config?.deployment || config?.style.textLabel) || 'Local';
+		let parsedConfig = config;
+		if(typeof config === 'string'){
+			parsedConfig = JSON.parse(config);
+		}
+
+		if(parsedConfig?.deployment || parsedConfig?.local) {
+			const bgText = (parsedConfig?.deployment || parsedConfig?.style.textLabel) || 'Local';
 			const styleObject = {
 				backgroundImage : `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' height='100px' width='200px'><text x='0' y='15' fill='%23fff7' font-size='20'>${bgText}</text></svg>")`,
-				backgroundColor : config?.style.backgroundColor ? `${config?.style.backgroundColor}` : null
+				backgroundColor : parsedConfig?.style.backgroundColor ? `${parsedConfig?.style.backgroundColor}` : null
 			};
-			console.table(config);
+			console.table(parsedConfig);
 			return styleObject;
 		}
 		return null;
