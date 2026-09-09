c4a41562f04a89bd6fb84c6ce62be66ce6f080d8
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2026-09-09 20:00:54 +0200

    undo server changes

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ async function start() {
 	const app = await createApp(vite);
 
 	const PORT = process.env.PORT || config.get('web_port') || 3000;
-	app.listen(PORT, '0.0.0.0', ()=>{
+	app.listen(PORT, ()=>{
 		const reset = '\x1b[0m'; // Reset to default style
 		const bright = '\x1b[1m'; // Bright (bold) style
 		const cyan = '\x1b[36m'; // Cyan color
