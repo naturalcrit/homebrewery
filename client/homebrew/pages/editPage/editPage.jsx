da882d6f006c5961bf7a1f102b19b1b6261e57aa
Trevor Buckner <calculuschild@gmail.com>
2026-09-09 20:55:30 -0400

    Fix google transfer ownership popup priority
    Only show alertOwnershipToTransfer if:
    
    1) current user is logged in
    2) brew actually has authors
    3) current user is not author[0]
    
    In all other cases, it should prompt to log in with a google account instead.

--- client/homebrew/pages/editPage/editPage.jsx
+++ client/homebrew/pages/editPage/editPage.jsx
@@ -160,7 +160,7 @@ const EditPage = (props)=>{
 	};
 
 	const handleGoogleClick = ()=>{
-		if(global.account !== currentBrew.authors[0]) {
+		if(currentBrew.authors.length > 0 && global.account?.username !== currentBrew.authors[0]) {
 			setAlertOwnershipToTransfer(true);
 			return;
 		}
@@ -264,12 +264,11 @@ const EditPage = (props)=>{
 		<Nav.item className='googleDriveStorage' onClick={handleGoogleClick}>
 			<img src={googleDriveIcon} className={saveGoogle ? '' : 'inactive'} alt='Google Drive icon' />
 
-
-
 			{alertOwnershipToTransfer && (
 				<div className='errorContainer'>
-					You must be the Owner to transfer between the Homebrewery and Google Drive!
-					The owner of this file is {currentBrew.authors[0]}
+					You must be the Owner to transfer between the Homebrewery and Google Drive! 
+					The owner of this file is {currentBrew.authors[0]}.
+					<br></br>
 					<div className='confirm' onClick={closeAlerts}> Okay </div>
 				</div>
 			)}
