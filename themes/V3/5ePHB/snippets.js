a9be3d913dc6ac147ffb2b14b187cca845961ec9
Eric <eric.scheid@ironclad.com.au>
2026-09-14 22:29:03 +1000

    Revert "add named aliases for first-letter drop-cap style"
    This reverts commit c14d1833b15a1ebd6764ac2450f1511018201158.

--- themes/V3/5ePHB/snippets.js
+++ themes/V3/5ePHB/snippets.js
@@ -30,7 +30,6 @@ export default [
 				name : 'Tweak Drop Cap',
 				icon : 'fas fa-sliders-h',
 				gen  : dedent`/* Drop Cap settings */
-						.page .first-letter, .page .drop-cap,
 						.page h1 + p::first-letter {
 							font-family: SolberaImitationRemake;
 							font-size: 3.5cm;
