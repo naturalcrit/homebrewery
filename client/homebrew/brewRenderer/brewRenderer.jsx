220047ffc7b874a6f273da207c7bbbca86412225
Trevor Buckner <calculuschild@gmail.com>
2026-09-23 19:21:36 +0000

    stray syntax

--- client/homebrew/brewRenderer/brewRenderer.jsx
+++ client/homebrew/brewRenderer/brewRenderer.jsx
@@ -334,7 +334,7 @@ const BrewRenderer = (props)=>{
 			<Frame id='BrewRenderer'  title='Rendered Brew Content' initialContent={INITIAL_CONTENT}
 				style={{ width: '100%', height: '100%', visibility: state.visibility }}
 				contentDidMount={frameDidMount}
-				onClick={()=>{emitClick();}}
+				onClick={emitClick}
 				sandbox='allow-same-origin allow-modals allow-top-navigation'
 			>
 				<div className='brewRenderer'
